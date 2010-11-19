bad465b14f8cec0057a985169ad13f95550abdd6
slmaerte <slmaerte@311c580b-64e4-4b78-a0c6-1348b473333e>
2010-11-19 03:33:59 +0000

    nog stuk vergeten bij userprofile
    git-svn-id: http://modelbrouwers.nl/svn/brouwers/trunk@33 311c580b-64e4-4b78-a0c6-1348b473333e

--- brouwers/general/views.py
+++ brouwers/general/views.py
@@ -20,6 +20,11 @@ def profile(request):
 		
 		if forms['profileform'].is_valid():
 			forms['profileform'].save()
+			if forms['profileform'].cleaned_data['exclude_from_nomination']:
+				projects = Project.objects.filter(brouwers__iexact=request.user.get_profile().forum_nickname)
+				for project in projects:
+					project.rejected = True
+					project.save()
 
 		if forms['userform'].is_valid():
 			forms['userform'].save()
