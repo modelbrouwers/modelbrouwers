e0131b00957f0fc4705cb1796c95ef383f260b80
slmaerte <slmaerte@311c580b-64e4-4b78-a0c6-1348b473333e>
2010-11-28 19:02:56 +0000

    Secret santa uitbreiden
    git-svn-id: http://modelbrouwers.nl/svn/brouwers/trunk@42 311c580b-64e4-4b78-a0c6-1348b473333e

--- brouwers/secret_santa/views.py
+++ brouwers/secret_santa/views.py
@@ -1,4 +1,14 @@
-from django.http import HttpResponseRedirect
+from django.template import RequestContext
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render_to_response
+
+from brouwers.secret_santa.models import Participant
+
+from datetime import date
 
 def index(request):
-	return HttpResponseRedirect('/profile/')
+	year = date.today().year
+	participants = Participant.objects.all()
+	participants = participants.filter(year = year)
+	return render_to_response('secret_santa/base.html', RequestContext(request, {'participants': participants, 'year': year}))
+#	return HttpResponseRedirect('/profile/')
