3cf828f2b68c051362a6556e7c9c15efeb230968
slmaerte <slmaerte@311c580b-64e4-4b78-a0c6-1348b473333e>
2010-11-29 03:31:11 +0000

    import vergeten
    git-svn-id: http://modelbrouwers.nl/svn/brouwers/trunk@58 311c580b-64e4-4b78-a0c6-1348b473333e

--- brouwers/secret_santa/views.py
+++ brouwers/secret_santa/views.py
@@ -1,6 +1,7 @@
 from django.template import RequestContext
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response
+from django.contrib.auth.decorators import user_passes_test
 
 from brouwers.secret_santa.models import Participant, Couple
 
