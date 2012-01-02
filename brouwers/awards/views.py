235794a09fdb0c058d616273749bc02985daf5bc
slmaerte <slmaerte@311c580b-64e4-4b78-a0c6-1348b473333e>
2012-01-02 15:11:39 +0000

    bugfix 3
    git-svn-id: http://modelbrouwers.nl/svn/brouwers/trunk@161 311c580b-64e4-4b78-a0c6-1348b473333e

--- brouwers/awards/views.py
+++ brouwers/awards/views.py
@@ -132,8 +132,8 @@ def winners(request):
 	#year redirects
 	if year >= today.year-1:
 		if voting_enabled() and year == today.year-1:
-			messages.info(request, "Het stemmen loopt nog, u kan nog geen winnaars voor dit jaar bekijken. U bent omgeleid naar de tussenstand.")
-			return HttpResponseRedirect(reverse(scores))
+			messages.info(request, "Het stemmen loopt nog, u kan nog geen winnaars voor dit jaar bekijken.")
+			return render_to_response(request, 'awards/winners.html', {'year': year, 'data': None, 'form': form})
 		year = today.year-1
 		messages.info(request, "Ook wij kunnen helaas niet in de toekomst kijken... u ziet dus de resultaten van editie %s." % year)
 		return HttpResponseRedirect("%s?year=%s" % (reverse(winners), year))
