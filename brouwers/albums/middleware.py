b15dec76a08695119ad6eaae5cccdca2aa69e5d4
slmaerte <slmaerte@311c580b-64e4-4b78-a0c6-1348b473333e>
2012-08-17 21:24:10 +0000

    bugfixes met middleware
    git-svn-id: http://modelbrouwers.nl/svn/brouwers/trunk@245 311c580b-64e4-4b78-a0c6-1348b473333e

--- brouwers/albums/middleware.py
+++ brouwers/albums/middleware.py
@@ -3,6 +3,9 @@ from django.core.urlresolvers import reverse
 
 class UploadifyMiddleware(object):
     def process_request(self, request):
-        if (request.method == 'POST') and (request.path == reverse('brouwers.albums.ajax_views.uploadify')) and \
+        try:
+            if (request.method == 'POST') and (request.path == reverse('brouwers.albums.ajax_views.uploadify')) and \
                 request.POST.has_key(settings.SESSION_COOKIE_NAME):
-            request.COOKIES[settings.SESSION_COOKIE_NAME] = request.POST[settings.SESSION_COOKIE_NAME]
+                request.COOKIES[settings.SESSION_COOKIE_NAME] = request.POST[settings.SESSION_COOKIE_NAME]
+        except: #causes viewdoesnotexist errors FIXME
+            pass
