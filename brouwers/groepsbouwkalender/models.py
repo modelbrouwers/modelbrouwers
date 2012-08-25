c08d2cdb7a4a4587a338e3e875e431a3716320d2
Mytheny <Mytheny@311c580b-64e4-4b78-a0c6-1348b473333e>
2012-08-25 00:50:02 +0000

    begin groepsbouwkalender , validate error.
    git-svn-id: http://modelbrouwers.nl/svn/brouwers/trunk@248 311c580b-64e4-4b78-a0c6-1348b473333e

---
+++ brouwers/groepsbouwkalender/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+
+# Create your models here.
+FORUMGEDEELTE_CHOICES = (
+    ('g','ter land'),
+    ('l','ter lucht'),
+    ('z','ter zee'),
+    ('a', 'anders'),
+)
+
+STATUS_CHOICES = (
+    ('a','aangevraagd'),
+    ('l','lopend'),
+    ('g','goedgekeurd'),
+    ('d','afgekeurd'),
+)
+    
+DURATION_CHOICES = (
+    ('´1´,´1 maand´'),
+    ('´2´,´2 maanden´'),
+    ('´3´,´3 maanden´'),
+    ('´6´,´6 maanden´'),
+    ('´12´,´12 maanden´'),
+    
+class Groepsbouw(models.Model):
+    applicant = models.charfield(max_length=100,)   
+    buildname = models.CharField(max_length=200, unique=False)
+    forumpart = models.CharField(max_length=1, choices=FORUMGEDEELTE_CHOICES)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    duration = models.PositiveSmallIntegerField(default=6, choices=DURATION_CHOICES)
+    description = models.TextField(blank=True)
+    topiclink = models.URLField(max_length=500,)
+    status = models.CharField(max.langth=1, choices=STATUS_CHOICES)
+    opmerkingen = models.CharField(max_length=300)
+
+    
