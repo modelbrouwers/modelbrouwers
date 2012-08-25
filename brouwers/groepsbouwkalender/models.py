6fb20633394e0d77387eda939cc7426a8477d474
Mytheny <Mytheny@311c580b-64e4-4b78-a0c6-1348b473333e>
2012-08-25 01:08:33 +0000

    git-svn-id: http://modelbrouwers.nl/svn/brouwers/trunk@249 311c580b-64e4-4b78-a0c6-1348b473333e

--- brouwers/groepsbouwkalender/models.py
+++ brouwers/groepsbouwkalender/models.py
@@ -16,22 +16,23 @@ STATUS_CHOICES = (
 )
     
 DURATION_CHOICES = (
-    ('´1´,´1 maand´'),
-    ('´2´,´2 maanden´'),
-    ('´3´,´3 maanden´'),
-    ('´6´,´6 maanden´'),
-    ('´12´,´12 maanden´'),
+    (1,'1 maand'),
+    (2,'2 maanden'),
+    (3,'3 maanden'),
+    (6,'6 maanden'),
+    (12,'12 maanden'),
+)
     
 class Groepsbouw(models.Model):
-    applicant = models.charfield(max_length=100,)   
+    applicant = models.CharField(max_length=100)   
     buildname = models.CharField(max_length=200, unique=False)
     forumpart = models.CharField(max_length=1, choices=FORUMGEDEELTE_CHOICES)
     start_date = models.DateField()
     end_date = models.DateField()
     duration = models.PositiveSmallIntegerField(default=6, choices=DURATION_CHOICES)
     description = models.TextField(blank=True)
-    topiclink = models.URLField(max_length=500,)
-    status = models.CharField(max.langth=1, choices=STATUS_CHOICES)
+    topiclink = models.URLField(max_length=500)
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES)
     opmerkingen = models.CharField(max_length=300)
 
     
