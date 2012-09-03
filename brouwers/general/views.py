ac844111aabda0b6337e371bfd64b108d56964c3
slmaerte <slmaerte@311c580b-64e4-4b78-a0c6-1348b473333e>
2012-09-03 20:18:02 +0000

    mailing deftiger maken
    git-svn-id: http://modelbrouwers.nl/svn/brouwers/trunk@291 311c580b-64e4-4b78-a0c6-1348b473333e

--- brouwers/general/views.py
+++ brouwers/general/views.py
@@ -103,7 +103,10 @@ def custom_login(request):
                         url = "http://%s%s"% (domain, reverse(confirm_account))
                         url_a = "<a href=\"%s\">%s</a>" % (url, url)
                         text_content = "Beste %s,\n\nUw code is: %s.\nGeef deze code in op: %s\n\nMvg,\nHet beheer" % (username, h, url)
-                        html_content = "Beste %s,<br /><br />Uw code is: <strong>%s</strong>.<br />Geef deze code in op: %s<br /><br />Mvg,<br />Het beheer" % (username, h, url_a)
+                        html_content = "<p>Beste %s,</p><br >"
+                        html_content += "<p>Uw code is: <strong>%s</strong>.</p>"
+                        html_content += "<p>Geef deze code in op: %s</p><br >"
+                        html_content += "<p>Mvg,</p><p>Het beheer</p>" % (username, h, url_a)
                         subject, from_email = 'Modelbrouwersaccount', 'beheer@modelbrouwers.nl'
                         
                         msg = EmailMultiAlternatives(subject, text_content, from_email, [email])
