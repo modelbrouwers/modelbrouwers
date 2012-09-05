686003e14bbdc731f601f67691d4ff763e409666
slmaerte <slmaerte@311c580b-64e4-4b78-a0c6-1348b473333e>
2012-09-05 20:56:49 +0000

    default homepage redirect
    git-svn-id: http://modelbrouwers.nl/svn/brouwers/trunk@306 311c580b-64e4-4b78-a0c6-1348b473333e

--- brouwers/general/urls.py
+++ brouwers/general/urls.py
@@ -1,11 +1,7 @@
+from django.conf import settings
 from django.conf.urls.defaults import *
 
-urlpatterns = patterns('django.views.generic.simple',
-#    (r'^$', 'direct_to_template', {'template': 'base.html'}),
-    (r'^phpBB3/$', 'redirect_to', {'url': '/phpBB3'}),
-    )
-
-urlpatterns += patterns('brouwers.general.views',
+urlpatterns = patterns('brouwers.general.views',
 #    (r'^$', 'index'),
     (r'^login/$', 'custom_login'),
     (r'^logout/$', 'custom_logout'),
@@ -18,4 +14,14 @@ urlpatterns += patterns('brouwers.general.views',
 urlpatterns += patterns('django.contrib.auth.views',
     (r'^profile/change_password/$', 'password_change', {'template_name':'general/password.html'}),
     (r'^password_change_done/$','password_change_done', {'template_name': 'general/password_change_done.html'}),
+    )
+
+if not settings.DEVELOPMENT:
+	urlpatterns += patterns('django.views.generic.simple',
+	#    (r'^$', 'direct_to_template', {'template': 'base.html'}),
+		(r'^$', 'redirect_to', {'url': '/phpBB3'}),
+		)
+else:
+	urlpatterns += patterns('brouwers.general.views',
+    	(r'^$', 'index'),
     )
