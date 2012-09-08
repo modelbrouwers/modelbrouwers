54ec097fde91d39751a37908f5ed85c5deff55f5
slmaerte <slmaerte@311c580b-64e4-4b78-a0c6-1348b473333e>
2012-09-08 18:17:48 +0000

    bugfix
    git-svn-id: http://modelbrouwers.nl/svn/brouwers/trunk@315 311c580b-64e4-4b78-a0c6-1348b473333e

--- brouwers/general/urls.py
+++ brouwers/general/urls.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('brouwers.general.views',
-#    (r'^$', 'index'),
+    (r'^$', 'index'),
     (r'^login/$', 'custom_login'),
     (r'^logout/$', 'custom_logout'),
     (r'^register/$', 'register'),
@@ -14,14 +14,4 @@ urlpatterns = patterns('brouwers.general.views',
 urlpatterns += patterns('django.contrib.auth.views',
     (r'^profile/change_password/$', 'password_change', {'template_name':'general/password.html'}),
     (r'^password_change_done/$','password_change_done', {'template_name': 'general/password_change_done.html'}),
-    )
-
-if not settings.DEVELOPMENT:
-	urlpatterns += patterns('django.views.generic.simple',
-	#    (r'^$', 'direct_to_template', {'template': 'base.html'}),
-		(r'^$', 'redirect_to', {'url': '/index.php'}),
-		)
-else:
-	urlpatterns += patterns('brouwers.general.views',
-    	(r'^$', 'index'),
     )
