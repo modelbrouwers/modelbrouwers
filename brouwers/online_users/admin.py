281cf33f25d179cb7f9a779774a6a2ef74512247
slmaerte <slmaerte@311c580b-64e4-4b78-a0c6-1348b473333e>
2012-09-20 20:16:43 +0000

    admin list edit
    git-svn-id: http://modelbrouwers.nl/svn/brouwers/trunk@340 311c580b-64e4-4b78-a0c6-1348b473333e

--- brouwers/online_users/admin.py
+++ brouwers/online_users/admin.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 
 class TrackedUserAdmin(admin.ModelAdmin):
     list_display = ('__unicode__', 'last_seen', 'notificate', 'is_online')
+    list_editable = ('notificate',)
     list_filter = ('last_seen', 'notificate')
     search_fields = ('user__first_name', 'user__last_name', 'user__email')
 
