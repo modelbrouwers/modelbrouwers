74eb8627f5637d17aa5889072f2761bcb4adbbe3
slmaerte <slmaerte@311c580b-64e4-4b78-a0c6-1348b473333e>
2012-11-14 21:26:59 +0000

    indent
    git-svn-id: http://modelbrouwers.nl/svn/brouwers/trunk@470 311c580b-64e4-4b78-a0c6-1348b473333e

--- brouwers/secret_santa/admin.py
+++ brouwers/secret_santa/admin.py
@@ -2,13 +2,13 @@ from django.contrib import admin
 from brouwers.secret_santa.models import *
 
 class ParticipantAdmin(admin.ModelAdmin):
-	list_display = ('user', 'year', 'verified')
+    list_display = ('user', 'year', 'verified')
     list_editable = ('verified',)
     list_filter = ('user',)
     search_fields = ('user__username', 'user__email', 'user__first_name', 'user__last_name')
 
 class CoupleAdmin(admin.ModelAdmin):
-	list_display = ('sender', 'receiver')
+    list_display = ('sender', 'receiver')
 
 admin.site.register(Participant, ParticipantAdmin)
 admin.site.register(Couple, CoupleAdmin)
