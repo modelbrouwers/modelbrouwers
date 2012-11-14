fda06fbc2d7d5a167b13c0cbcb621019a2c8f6a0
slmaerte <slmaerte@311c580b-64e4-4b78-a0c6-1348b473333e>
2012-11-14 21:40:54 +0000

    small updates
    git-svn-id: http://modelbrouwers.nl/svn/brouwers/trunk@471 311c580b-64e4-4b78-a0c6-1348b473333e

--- brouwers/secret_santa/admin.py
+++ brouwers/secret_santa/admin.py
@@ -4,7 +4,7 @@ from brouwers.secret_santa.models import *
 class ParticipantAdmin(admin.ModelAdmin):
     list_display = ('user', 'year', 'verified')
     list_editable = ('verified',)
-    list_filter = ('user',)
+    list_filter = ('year', 'verified')
     search_fields = ('user__username', 'user__email', 'user__first_name', 'user__last_name')
 
 class CoupleAdmin(admin.ModelAdmin):
