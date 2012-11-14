6128d6f088ee466f6b8a228d2bc15793119456e0
slmaerte <slmaerte@311c580b-64e4-4b78-a0c6-1348b473333e>
2012-11-14 21:24:45 +0000

    kleine admin wijzigingen
    git-svn-id: http://modelbrouwers.nl/svn/brouwers/trunk@469 311c580b-64e4-4b78-a0c6-1348b473333e

--- brouwers/secret_santa/admin.py
+++ brouwers/secret_santa/admin.py
@@ -3,6 +3,9 @@ from brouwers.secret_santa.models import *
 
 class ParticipantAdmin(admin.ModelAdmin):
 	list_display = ('user', 'year', 'verified')
+    list_editable = ('verified',)
+    list_filter = ('user',)
+    search_fields = ('user__username', 'user__email', 'user__first_name', 'user__last_name')
 
 class CoupleAdmin(admin.ModelAdmin):
 	list_display = ('sender', 'receiver')
