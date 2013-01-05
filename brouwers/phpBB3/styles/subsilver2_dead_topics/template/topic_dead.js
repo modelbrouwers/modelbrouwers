5002e4bfe160f91eef9cfa2b79e896268a049b2c
slmaerte <slmaerte@311c580b-64e4-4b78-a0c6-1348b473333e>
2013-01-05 21:54:53 +0000

    custom style erbij
    git-svn-id: http://modelbrouwers.nl/svn/brouwers/trunk@533 311c580b-64e4-4b78-a0c6-1348b473333e

---
+++ brouwers/phpBB3/styles/subsilver2_dead_topics/template/topic_dead.js
@@ -0,0 +1,21 @@
+function test_url(topic_id, a){
+	$.get('./ajax/topic_dead.php', {t: topic_id}, function(data){
+		if (data == ""){
+			var url = "" + a.attr('href');
+			window.location=url;
+		} else {
+			$('body').css('overflow-y','hidden');
+			$('div#blanket').show();
+			$('div#dead_topic').show();
+			$('div#message_topic_dead').html(data);
+		}
+	});
+}
+$(document).ready(function() {
+	$('a#close_message').click(function(){
+		$('div#blanket').hide();
+		$('div#dead_topic').hide();
+		$('body').css('overflow-y','auto');
+		return false;
+	});
+});
