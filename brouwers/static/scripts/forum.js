f6ea22c51e1e408a7d8395f7460f8fa30c0b905c
slmaerte <slmaerte@311c580b-64e4-4b78-a0c6-1348b473333e>
2013-01-06 13:18:53 +0000

    svn ignore toegepast op phpBB3 folder
    git-svn-id: http://modelbrouwers.nl/svn/brouwers/trunk@540 311c580b-64e4-4b78-a0c6-1348b473333e

--- brouwers/static/scripts/forum.js
+++ brouwers/static/scripts/forum.js
@@ -1,3 +1,26 @@
 $(document).ready(function(){
     $.get('/ou/so/');
+    
+    // dead topics
+    $('a#close_message').click(function(){
+		$('div#blanket').hide();
+		$('div#dead_topic').hide();
+		$('body').css('overflow-y','auto');
+		return false;
+	});
 });
+
+// dead topics
+function test_url(topic_id, a){
+	$.get('./ajax/topic_dead.php', {t: topic_id}, function(data){
+		if (data == ""){
+			var url = "" + a.attr('href');
+			window.location=url;
+		} else {
+			$('body').css('overflow-y','hidden');
+			$('div#blanket').show();
+			$('div#dead_topic').show();
+			$('div#message_topic_dead').html(data);
+		}
+	});
+}
