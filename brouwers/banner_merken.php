6bcada8ba0b820c434f46f0814ca5ce690e41a47
bbt <bbt@311c580b-64e4-4b78-a0c6-1348b473333e>
2013-04-12 08:58:21 +0000

    post commit testen
    git-svn-id: http://modelbrouwers.nl/svn/brouwers/trunk@628 311c580b-64e4-4b78-a0c6-1348b473333e

---
+++ brouwers/banner_merken.php
@@ -0,0 +1,25 @@
+<center>
+    <table width="1250" border="0" align="center">
+      <tr>
+        <td><a href="http://www.modelbrouwers.nl/shop/catalog/uhu-m-28.html?language=nl&amp;sort=2a"><img src="images/box2/uhu.gif" alt="UHU" width="54" height="25" border="0" /></a></td>
+        <td><a href="http://www.modelbrouwers.nl/shop/catalog/xuron-m-52.html?language=nl&amp;sort=2a"><img src="images/box2/xuron.gif" alt="Xuron" width="102" height="25" border="0" /></a></td>
+        <td><a href="http://www.modelbrouwers.nl/shop/catalog/revell-m-14.html?language=nl&amp;sort=3d"><img src="images/box2/revell.gif" alt="Revell" width="62" height="25" border="0" /></a></td>
+        <td><a href="http://www.modelbrouwers.nl/shop/catalog/alclad2-m-19.html?language=nl&amp;sort=2a"><img src="images/box2/alclad.gif" alt="Alclad" width="65" height="25" border="0" /></a></td>
+        <td><a href="http://www.modelbrouwers.nl/shop/catalog/bare-metal-foil-m-15.html?language=nl&amp;sort=2a"><img src="images/box2/bmf.gif" alt="Bare Metal Foil" width="66" height="25" border="0" /></a></td>
+        <td><a href="http://www.modelbrouwers.nl/shop/catalog/birchwood-casey-m-44.html?language=nl&amp;sort=2a"><img src="images/box2/birchwood_casey.gif" alt="Birchwood Casey" width="52" height="25" border="0" /></a></td>
+        <td><a href="http://www.modelbrouwers.nl/shop/catalog/createx-m-23.html?language=nl&amp;sort=2a"><img src="images/box2/createx.gif" alt="Createx" width="54" height="25" border="0" /></a></td>
+        <td><a href="http://www.modelbrouwers.nl/shop/catalog/evergreen-m-11.html?language=nl&amp;sort=2a"><img src="images/box2/evergreen.gif" alt="Evergreen" width="50" height="25" border="0" /></a></td>
+        <td><a href="http://www.modelbrouwers.nl/shop/catalog/billing-boats-m-29.html?language=nl&amp;sort=2a"><img src="images/box2/billing.gif" alt="Billing Boats" width="41" height="25" border="0" /></a></td>
+        <td><a href="http://www.modelbrouwers.nl/shop/catalog/excel-m-35.html?language=nl&amp;sort=2a"><img src="images/box2/excel.gif" alt="Excel" width="56" height="25" border="0" /></a></td>
+        <td><a href="http://www.modelbrouwers.nl/shop/catalog/rc-afstandbestuurbaar-rcsets-billing-boats-c-106_120.html?language=nl&amp;sort=2a"><img src="images/box2/graupner.gif" alt="Graupner" width="65" height="25" border="0" /></a></td>
+        <td><a href="http://www.modelbrouwers.nl/shop/catalog/mamoli-m-32.html?language=nl&amp;sort=2a"><img src="images/box2/mamoli.gif" alt="Mamoli" width="82" height="25" border="0" /></a></td>
+        <td><a href="http://www.modelbrouwers.nl/shop/catalog/microscale-m-17.html?language=nl&amp;sort=2a"><img src="images/box2/microscale.gif" alt="Microscale" width="62" height="25" border="0" /></a></td>
+        <td><a href="http://www.modelbrouwers.nl/shop/catalog/milliput-m-26.html?language=nl&amp;sort=2a"><img src="images/box2/milliput.gif" alt="Milliput" width="93" height="25" border="0" /></a></td>
+        <td><a href="http://www.modelbrouwers.nl/shop/catalog/modelbrouwers-m-12.html?language=nl&amp;page=1&amp;sort=3d"><img src="images/box2/modelbrouwers.gif" alt="Modelbrouwers" width="127" height="25" border="0" /></a></td>
+        <td><a href="http://www.modelbrouwers.nl/shop/catalog/rustall-m-53.html?language=nl&amp;sort=2a"><img src="images/box2/rustall.gif" alt="Rustall" width="93" height="25" border="0" /></a></td>
+        <td><a href="http://www.modelbrouwers.nl/shop/catalog/scalemotorsport-m-24.html?language=nl&amp;sort=2a"><img src="images/box2/scalemotorsport.gif" alt="Scale Motorsport" width="25" height="25" border="0" /></a></td>
+        <td><a href="http://www.modelbrouwers.nl/shop/catalog/pro-modeller-m-43.html?language=nl&amp;sort=2a"><img src="images/box2/promodeller.gif" alt="" width="57" height="25" border="0" /></a></td>
+        <td><a href="http://www.modelbrouwers.nl/shop/catalog/vallejo-m-42.html?language=nl&amp;sort=2a"><img src="images/box2/vallejo.gif" alt="Vallejo" width="42" height="25" border="0" /></a></td>
+      </tr>
+    </table>
+</center>
