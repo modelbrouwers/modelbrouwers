edcb030bc96dcf7ce6b9b64121a8f6086403f578
bbt <bbt@311c580b-64e4-4b78-a0c6-1348b473333e>
2013-05-27 11:27:18 +0000

    added auth backend for phpBB3
    git-svn-id: http://modelbrouwers.nl/svn/brouwers/trunk@637 311c580b-64e4-4b78-a0c6-1348b473333e

--- brouwers/manage.py
+++ brouwers/manage.py
@@ -2,6 +2,8 @@
 import os, sys
 
 if __name__ == "__main__":
+    sys.path.append('/home/modelbrouw/brouwers/')
+    #sys.path.append('/home/modelbrouw/brouwers/brouwers/')
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "brouwers.settings")
     from django.core.management import execute_from_command_line
     
