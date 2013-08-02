a25f684012169b406ebea63b822578c6cd9558d2
bbt <bbt@311c580b-64e4-4b78-a0c6-1348b473333e>
2013-08-02 18:34:07 +0000

    testje
    git-svn-id: http://modelbrouwers.nl/svn/brouwers/trunk@723 311c580b-64e4-4b78-a0c6-1348b473333e

---
+++ brouwers/utils/bbcode.py
@@ -0,0 +1,12 @@
+#TODO: check existing packages...
+
+import re
+
+BOLD_REGEX = re.compile(r'\[b\](?P<bold_text>(.)*)\[\/b\]')
+BOLD_CLASS = 'bold'
+
+def parse(text):
+    pass
+
+def parse_bold(text):
+    pass
