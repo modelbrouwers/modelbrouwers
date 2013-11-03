3af9446ac924b172a0fd862199ce6481b2b316a5
Sergei Maertens <sergei@maykinmedia.nl>
2013-11-03 20:39:53 +0100

    clientside validation

--- builds/static/js/builds.js
+++ builds/static/js/builds.js
@@ -1,3 +1,5 @@
+var img_extensions = ['jpg', 'jpeg', 'png'];
+
 $(document).ready(function(){
 	$('#id_search_term').autocomplete({
 		source: search_url,
@@ -5,8 +7,8 @@ $(document).ready(function(){
 		minLength: 3,
 		delay: 0,
 		select:  function(e, ui) {
-            window.location = ui.item.url;
-        }
+			window.location = ui.item.url;
+		}
 	});
 
 	$('div.kit-info').hide();
@@ -20,26 +22,67 @@ $(document).ready(function(){
 	});
 
 	$('input.order, input.photo-url').tooltip({
-    	track: true
-    });
+		track: true
+	});
 
 	// drag and drop photos
 	$('#radio').buttonset();
-    $('#add-albumphoto').click(show_photos_selector);
-    $('#photos-list').on('change', '#id_album', function(){
-    	$('#photos-list').data('current-album', $(this).val());
-    	update_photos_selector();
-    });
+	$('#add-albumphoto').click(show_photos_selector);
+	$('#photos-list').on('change', '#id_album', function(){
+		$('#photos-list').data('current-album', $(this).val());
+		update_photos_selector();
+	});
 
-    /* links to photos */
-    // hide last 9 blocks if they're empty
-    $('li.photo-form:not(:first)').filter(function(index){
-    	return !$(this).find('.photo-url').val();
-    }).hide();
+	/* links to photos */
+	// hide last 9 blocks if they're empty
+	$('li.photo-form:not(:first)').filter(function(index){
+		return !$(this).find('.photo-url').val();
+	}).hide();
+	// hide order fields, they're set already
+	$('.order').hide();
 
-    $('#photos-formset').on('keyup', 'input.photo-url', function(){
-    	update_photo_fields($(this));
-    });
+	// update preview & do validation
+	$('#photos-formset').on('keyup', 'input.photo-url', function(event){
+		var img_src = $(this).val();
+		var extension = img_src.split('.').pop();
+		var preview = $(this).closest('fieldset').siblings('.preview');
+		$(this).siblings('.error.jquery-validation').remove();
+
+		if (img_extensions.indexOf(extension.toLowerCase()) > -1){
+			var tpl = load_template('photo_preview');
+			var context = {'img_src': img_src};
+			var rendered_tpl = tpl(context);
+			preview.html(rendered_tpl);
+			update_photo_fields($(this));
+
+			if($('.error.jquery-validation').length == 0){
+				// allow submitting
+				$(this).closest('form').unbind('submit');
+				$('#submit-build').removeAttr('disabled');
+			}
+		} else {
+			preview.html('');
+			update_photo_fields($(this), false);
+			if($(this).val()){
+				// show invalid warning
+				var tpl = load_template('invalid_img_url');
+				$(tpl()).insertAfter($(this));
+				// disable form
+				$(this).closest('form').submit(function(event){
+					event.preventDefault();
+					return false;
+				});
+				// disable button
+				$('#submit-build').prop('disabled', true);
+			} else {
+				if($('.error.jquery-validation').length == 0){
+				// allow submitting
+					$(this).closest('form').unbind('submit');
+					$('#submit-build').removeAttr('disabled');
+				}
+			}
+		}
+	});
 });
 
 
@@ -71,14 +114,14 @@ function update_photos_selector(){
 	console.log('I am updating');
 }
 
-function update_photo_fields(url_input){
+function update_photo_fields(url_input, show_next){
 	var li_item = url_input.closest('li.photo-form');
 	var next_li = li_item.next();
 	
 	if(url_input.val()){
 		// something is filled in, so we add a line
 		li_item.data('used', true);
-		if(!next_li.is(':visible')){
+		if(!next_li.is(':visible') && show_next){
 			// fix the order
 			if(next_li.find('.order').val() == 1){
 				var order = next_li.data('order');
