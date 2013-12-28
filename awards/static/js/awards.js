8feb82ea74a33627e8c5a49f1fda2f7d25db8041
Sergei Maertens <sergeimaertens@gmail.com>
2013-12-28 23:35:43 +0100

    Drag and drop implemented

--- awards/static/js/awards.js
+++ awards/static/js/awards.js
@@ -1,3 +1,22 @@
 $(function(){
-	// do stuff
+	$('.voteable').closest('li.project').draggable({
+		// helper: 'clone',
+		revert: 'invalid'
+	});
+
+	$('div.vote').droppable({
+		accept: 'li.project',
+		hoverClass: 'highlight-border',
+		drop: function(event, ui){
+			ui.draggable.addClass('dropped');
+			ui.draggable.appendTo($(this).find('.vote-accept'));
+			ui.draggable.css('top', '0').css('left', '0');
+			// prevent other elements to be dropped
+			$(this).droppable('option', 'accept', ui.draggable);
+		},
+		out: function(event, ui){
+	        $(this).droppable('option', 'accept', 'li.project');
+        }
+		// TODO: scope
+	});
 });
