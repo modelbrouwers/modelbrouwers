03527284960f76c46a30e2e906c80cd85c5308f8
Sergei Maertens <sergeimaertens@gmail.com>
2013-12-29 17:09:08 +0100

    Sprite icons start, fixed form contents, fixed drag and drop

--- awards/static/js/awards.js
+++ awards/static/js/awards.js
@@ -1,38 +1,57 @@
 $(function(){
-	$('.voteable').closest('li.project').draggable({
-		// helper: 'clone',
-		revert: 'invalid'
+	$('.voteable').closest('li.project').each(function(){
+		var scope = $(this).closest('div.category').find('input.category').attr('name');
+		$(this).draggable({
+			containment: $(this).closest('div.category'),
+			revert: 'invalid',
+			revertDuration: 100,
+			cursorAt: {
+				bottom: 0,
+				right: 0
+			},
+			scope: scope
+		});
 	});
 
-	$('div.vote').droppable({
-		accept: 'li.project',
-		hoverClass: 'highlight-border',
-		drop: function(event, ui){
-			ui.draggable.addClass('dropped');
-			ui.draggable.appendTo($(this).find('.vote-accept'));
-			ui.draggable.css('top', '0').css('left', '0');
-			// prevent other elements to be dropped
-			$(this).droppable('option', 'accept', ui.draggable);
+	$('div.vote').each(function(){
+		var scope = $(this).closest('div.category').find('input.category').attr('name');
+		$(this).droppable({
+			accept: 'li.project',
+			hoverClass: 'highlight-border',
+			drop: function(event, ui){
+				ui.draggable.addClass('dropped');
+				ui.draggable.appendTo($(this).find('.vote-accept'));
+				ui.draggable.css('top', '0').css('left', '0');
+				// prevent other elements to be dropped
+				$(this).droppable('option', 'accept', ui.draggable);
 
-			setVote(ui.draggable.data('id'), $(this));
-		},
-		out: function(event, ui){
-	        $(this).droppable('option', 'accept', 'li.project');
-        }
-		// TODO: scope
+				setVote(ui.draggable.data('id'), $(this));
+			},
+			out: function(event, ui){
+				$(this).droppable('option', 'accept', 'li.project');
+			},
+			scope: scope
+		});
 	});
+
+	$('a.reset').click(function(event){
+		event.preventDefault();
+		var container = $(this).closest('div.category');
+		var voteDiv = $(this).closest('.vote');
+		var destClass = container.data('input-class');
+		var projectList = container.find('ul.projects-list');
+
+		container.find('input.'+destClass).val('');
+		voteDiv.find('ul.vote-accept li').appendTo(projectList);
+		projectList.find('.dropped').removeClass('dropped');
+		// let it accept all projects
+		voteDiv.droppable('option', 'accept', 'li.project');
+		return false;
+	})
 });
 
 function setVote(projectId, droppable){
-	var destClass;
-	if(droppable.hasClass('first')){
-		destClass = 'project1';
-	} else if(droppable.hasClass('second')) {
-		destClass = 'project2';
-	} else if(droppable.hasClass('third')) {
-		destClass = 'project3';
-	}
-
+	var destClass = droppable.data('inputClass');
 	// set the value
 	droppable.closest('div.vote-blocks').siblings('input.'+destClass).val(projectId);
 }
