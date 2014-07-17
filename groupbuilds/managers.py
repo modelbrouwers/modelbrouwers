b81d2eb9f06eb3e5c33c270cd0d2b719835c9db6
Sergei Maertens <sergeimaertens@gmail.com>
2014-07-17 22:00:12 +0200

    Fixed bug with empty end date. Long live unit tests!

--- groupbuilds/managers.py
+++ groupbuilds/managers.py
@@ -11,6 +11,6 @@ class PublicGroupBuildsManager(Manager):
         base_qs = super(PublicGroupBuildsManager, self).get_query_set()
         from .models import GroupbuildStatuses # avoid circular imports
         date_limit = timezone.now() - timedelta(days=14)
-        q_end_date = Q(Q(end__gte=date_limit) & Q(status__in=GroupbuildStatuses.date_bound_statuses))
+        q_end_date = Q(Q(Q(end__gte=date_limit) | Q(end=None)) & Q(status__in=GroupbuildStatuses.date_bound_statuses))
         q_end_date |= Q(status__in=GroupbuildStatuses.non_date_bound_statuses)
         return base_qs.filter(status__in=GroupbuildStatuses.public_statuses).filter(q_end_date)
