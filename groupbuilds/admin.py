15aca1d759f677e5938886ff820e387fe123ecc0
Sergei Maertens <sergeimaertens@gmail.com>
2014-07-26 14:17:09 +0200

    Admin improvements

--- groupbuilds/admin.py
+++ groupbuilds/admin.py
@@ -5,6 +5,7 @@ from .models import GroupBuild, Participant
 
 class InlineParticipant(admin.TabularInline):
     model = Participant
+    raw_id_fields = ('user',)
 
 
 class GroupBuildAdmin(admin.ModelAdmin):
@@ -12,8 +13,10 @@ class GroupBuildAdmin(admin.ModelAdmin):
     list_editable = ('start', 'end', 'status')
     list_filter = ('status', 'start', 'end', 'category', 'created', 'modified')
     search_fields = ('theme', 'description', 'applicant__username')
-
+    filter_horizontal = ('admins',)
+    raw_id_fields = ('applicant',)
     inlines = [InlineParticipant]
+    prepopulated_fields = {'slug': ('theme',)}
 
 
 admin.site.register(GroupBuild, GroupBuildAdmin)
