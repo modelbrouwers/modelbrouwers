d6d11f86a7763a99d05bc92bd63810352c2a433b
Sergei Maertens <sergei-maertens@users.noreply.github.com>
2014-07-28 21:08:57 +0200

    Update admin.py
    Search on ip address

--- banning/admin.py
+++ banning/admin.py
@@ -5,7 +5,7 @@ from models import *
 class BanAdmin(admin.ModelAdmin):
     list_display = ('user', 'ip', 'expiry_date', 'automatic', 'registrationattempt')
     list_filter = ('automatic', 'expiry_date',)
-    search_fields = ('user__username', 'registrationattempt__username')
+    search_fields = ('user__username', 'registrationattempt__username', 'ip')
 
 
 admin.site.register(Ban, BanAdmin)
