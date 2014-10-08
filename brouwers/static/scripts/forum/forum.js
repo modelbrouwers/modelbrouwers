6c8d7f152241c9e916b3d2f6e02ec665f51fdbd0
sergei-maertens <sergeimaertens@gmail.com>
2014-10-08 23:56:09 +0200

    Try to fix some javascript troubles

--- brouwers/static/scripts/forum/forum.js
+++ brouwers/static/scripts/forum/forum.js
@@ -98,10 +98,10 @@ $(document).ready(function() {
 
 /* Implement a string formatter */
 if (!String.prototype.format) {
-  String.prototype.format = function() {
-    var args = arguments;
-    return this.replace(/{(\d+)}/g, function(match, number) {
-      return typeof args[number] != 'undefined' ? args[number] : match ;
-    });
-  };
+	String.prototype.format = function() {
+		var args = arguments;
+		return this.replace(/{(\d+)}/g, function(match, number) {
+			return typeof args[number] != 'undefined' ? args[number] : match ;
+		});
+	};
 }
