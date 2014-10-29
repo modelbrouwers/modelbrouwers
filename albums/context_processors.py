d9d671fb84e8f022e0296b8e2fc6268a4dd2d862
Sergei Maertens <sergeimaertens@gmail.com>
2014-10-29 20:47:17 +0100

    Simplified context processor + added test for anonymous user

--- albums/context_processors.py
+++ albums/context_processors.py
@@ -4,13 +4,7 @@ from .utils import admin_mode
 
 def user_is_album_admin(request):
     p = Preferences.get_or_create(request.user)
-
-    is_album_admin = False
-    try:
-        if admin_mode(request.user, preferences=p):
-            is_album_admin = True
-    except TypeError: #anonymous user
-        pass
+    is_album_admin = admin_mode(request.user, preferences=p)
     return {
         'user_is_album_admin': is_album_admin,
         'album_preferences': p
