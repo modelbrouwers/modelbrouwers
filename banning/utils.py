97dce3de5cf054ce92cc001eee6d531b943d9161
Sergei Maertens <sergeimaertens@gmail.com>
2015-01-19 15:11:14 +0100

    Fix stupid mistake

--- banning/utils.py
+++ banning/utils.py
@@ -13,5 +13,5 @@ def set_banning_cache(qs=None):
     for ban in [ban for ban in bans if
                 ban.expiry_date is not None
                 and ban.expiry_date.tzinfo is None]:
-        bans.expiry_date.replace(tzinfo=timezone.utc)
+        ban.expiry_date = ban.expiry_date.replace(tzinfo=timezone.utc)
     cache.set(CACHE_KEY, bans, 60*60*24) # cache 24 hours
