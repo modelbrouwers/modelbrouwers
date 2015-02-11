cdcd987283be431d8650b666df0b9216795c3a98
Sergei Maertens <sergeimaertens@gmail.com>
2015-02-11 09:33:30 +0100

    Compiled translations

---
+++ src/brouwers/utils/management/commands/compile_project_messages.py
@@ -0,0 +1,22 @@
+import os
+
+from django.conf import settings
+from django.core.management import call_command
+from django.core.management.base import NoArgsCommand
+
+
+class Command(NoArgsCommand):
+
+    requires_model_validation = False
+
+    def handle_noargs(self, *args, **options):
+        os.chdir(settings.PROJECT_DIR)
+        call_command('compilemessages')
+
+        dirs = os.listdir(os.path.join(settings.PROJECT_DIR, 'brouwers'))
+        for app in dirs:
+            app_path = os.path.join(settings.PROJECT_DIR, 'brouwers', app)
+            locale_path = os.path.join(app_path, "locale")
+            if(os.path.exists(locale_path)):
+                os.chdir(app_path)
+                call_command('compilemessages')
