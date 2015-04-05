5bda6312104337516b65b216dc8c9b15f0072f77
Sergei Maertens <sergeimaertens@gmail.com>
2015-04-05 22:45:43 +0200

    Got working uploads

--- src/brouwers/albums/api/serializers.py
+++ src/brouwers/albums/api/serializers.py
@@ -6,3 +6,4 @@ from ..models import Photo
 class PhotoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Photo
+        fields = ('album', 'image', 'description')
--- src/brouwers/albums/api/views.py
+++ src/brouwers/albums/api/views.py
@@ -1,7 +1,11 @@
 from rest_framework import parsers, permissions
+from rest_framework import status
 from rest_framework import viewsets
+from rest_framework.response import Response
 
 from ..models import Photo
+from .serializers import PhotoSerializer
+from .renderers import FineUploaderRenderer
 
 
 class PhotoViewSet(viewsets.ModelViewSet):
@@ -10,4 +14,23 @@ class PhotoViewSet(viewsets.ModelViewSet):
     """
     permission_classes = (permissions.IsAuthenticated,)
     parser_classes = (parsers.FileUploadParser,)
-    queryset = Photo.objects.all()
+    queryset = Photo.objects.none()
+    serializer_class = PhotoSerializer
+
+    def get_renderers(self):
+        if self.request.method == 'POST':
+            return [FineUploaderRenderer()]
+        return super(PhotoViewSet, self).get_renderers()
+
+    def create(self, request, *args, **kwargs):
+        """
+        Overwritten to comply with FineUploader's demands for the response.
+        """
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save(user=self.request.user)
+            headers = self.get_success_headers(serializer.data)
+            response_data = serializer.data
+            response_data.update({'success': True})
+            return Response(response_data, status=status.HTTP_200_OK, headers=headers)
+        return Response({'success': False}, status=status.HTTP_400_BAD_REQUEST)
