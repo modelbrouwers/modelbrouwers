7970effb19548e4a7e4f7e9802078dea4d23ff34
Sergei Maertens <sergeimaertens@gmail.com>
2015-05-19 23:50:13 +0200

    Try to merge migrations

---
+++ src/brouwers/albums/migrations/0006_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('albums', '0003_auto_20150507_2216'),
+        ('albums', '0005_auto_20150408_0913'),
+    ]
+
+    operations = [
+    ]
