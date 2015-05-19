bc1f1bca97f69d99fb6ad7ea07196839076663d2
Sergei Maertens <sergeimaertens@gmail.com>
2015-05-19 23:02:31 +0200

    OOP controls

--- src/brouwers/albums/static/albums/js/photo-detail.js
+++ src/brouwers/albums/static/albums/js/photo-detail.js
@@ -1,23 +1,48 @@
 $(function() {
     'use strict';
 
-    $('.controls').on('click', '[data-action]', function(event) {
-        event.preventDefault();
-        var mutualExclusive,
-            $control = $(this),
-            $figure = $control.closest('figure');
+    var controls = {};
 
-        // remove active class from mutually exclusive controls
-        $control.siblings().removeClass('active');
-
-        // remove current action css class and add the correct class
-        mutualExclusive = $.map($control.siblings(), function(el) {
+    var Control = function($node, $target) {
+        this.node = $node;
+        this.target = $target;
+        this.action = $node.data('action');
+        this.mutualExclusiveActions = $.map($node.siblings(), function(el) {
             return $(el).data('action');
         });
-        $figure
-            .removeClass(mutualExclusive.join(' '))
-            .addClass($control.data('action'));
-        $control.addClass('active');
+    };
+
+    Control.prototype.activate = function() {
+        this.node.siblings().removeClass('active');
+        this.target
+            .removeClass(this.mutualExclusiveActions.join(' '))
+            .addClass(this.action);
+        this.node.addClass('active');
+    };
+
+    Control.prototype.deactivate = function() {
+        this.node.removeClass('active');
+        this.target.removeClass(this.action);
+    };
+
+    Control.prototype.toggle = function() {
+        if (this.node.hasClass('active')) {
+            this.deactivate();
+        } else {
+            this.activate();
+        }
+    };
+
+
+    $('.controls').on('click', '[data-action]', function(event) {
+        event.preventDefault();
+
+        var control,
+            action = $(this).data('action'),
+            $figure = $(this).closest('figure');
+
+        control = controls[action] || new Control($(this), $figure);
+        control.toggle();
         return false;
     });
 });
