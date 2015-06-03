687a3044df691e49ec03783434867afe0ce52b0a
Sergei Maertens <sergeimaertens@gmail.com>
2015-06-03 15:25:50 +0200

    fixed regular non-api views

--- src/brouwers/albums/urls.py
+++ src/brouwers/albums/urls.py
@@ -14,19 +14,17 @@ urlpatterns = patterns(
     '',
     url(r'^$', IndexView.as_view(), name='index'),
     url(r'^new/$', AlbumCreateView.as_view(), name='create'),
-    url(r'^list/$', RedirectView.as_view(pattern_name='albums:all')),
+    url(r'^list/$', RedirectView.as_view(pattern_name='albums:all', permanent=True)),
     url(r'^all/$', AlbumListView.as_view(), name='all'),
     url(r'^mine/$', MyAlbumsView.as_view(), name='mine'),
+    url(r'^my_gallery/$', RedirectView.as_view(pattern_name='albums:mine', permanent=True)),
     url(r'^album/\{?(?P<pk>\d+)\}?/$', AlbumDetailView.as_view(), name='detail'),  # curly braces for Javascript url
     url(r'^album/(?P<pk>\d+)/page/(?P<page>\d+)/$', AlbumDetailView.as_view(), name='detail'),
     url(r'^album/(?P<pk>\d+)/download/$', AlbumDownloadView.as_view(), name='download'),
     url(r'^photo/(?P<pk>\d+)/$', PhotoDetailView.as_view(), name='photo-detail'),
     url(r'^upload/$', UploadView.as_view(), name='upload'),
     url(r'^settings/$', PreferencesUpdateView.as_view(), name='settings'),
-
-
-    # (r'^my_gallery/$',      'my_albums_list'),
-    # (r'^preferences/$',     'preferences'),
+    (r'^preferences/$', RedirectView.as_view(pattern_name='albums:settings', permanent=True)),
 )
 
 # AJAX
