2db89e23543cd50823454a01a086414a8e534484
Sergei Maertens <sergeimaertens@gmail.com>
2015-06-25 00:24:05 +0200

    Generated depcache

--- src/static/config.js
+++ src/static/config.js
@@ -14,6 +14,109 @@ System.config({
 });
 
 System.config({
+  "depCache": {
+    "npm:pop-iterate@1.0.1/object-iterator": [
+      "npm:pop-iterate@1.0.1/iteration",
+      "npm:pop-iterate@1.0.1/array-iterator"
+    ],
+    "scripts/jquery.serializeObject": [
+      "github:components/jquery@2.1.4"
+    ],
+    "scripts/csrf": [
+      "github:components/jquery@2.1.4"
+    ],
+    "scripts/paginator": [
+      "github:components/jquery@2.1.4"
+    ],
+    "github:components/jquery@2.1.4": [
+      "github:components/jquery@2.1.4/jquery"
+    ],
+    "github:components/handlebars.js@2.0.0": [
+      "github:components/handlebars.js@2.0.0/handlebars"
+    ],
+    "npm:weak-map@1.0.5": [
+      "npm:weak-map@1.0.5/weak-map"
+    ],
+    "npm:pop-iterate@1.0.1/array-iterator": [
+      "npm:pop-iterate@1.0.1/iteration"
+    ],
+    "npm:process@0.10.1": [
+      "npm:process@0.10.1/browser"
+    ],
+    "scripts/api": [
+      "github:components/jquery@2.1.4",
+      "npm:q@2.0.3",
+      "scripts/strformat",
+      "scripts/jquery.serializeObject",
+      "scripts/csrf"
+    ],
+    "github:twbs/bootstrap@3.3.5/js/bootstrap": [
+      "github:components/jquery@2.1.4"
+    ],
+    "npm:pop-iterate@1.0.1/pop-iterate": [
+      "npm:pop-iterate@1.0.1/array-iterator",
+      "npm:pop-iterate@1.0.1/object-iterator"
+    ],
+    "github:jspm/nodelibs-process@0.1.1/index": [
+      "npm:process@0.10.1"
+    ],
+    "scripts/manager": [
+      "npm:q@2.0.3",
+      "scripts/api",
+      "scripts/paginator"
+    ],
+    "github:twbs/bootstrap@3.3.5": [
+      "github:twbs/bootstrap@3.3.5/js/bootstrap"
+    ],
+    "npm:pop-iterate@1.0.1": [
+      "npm:pop-iterate@1.0.1/pop-iterate"
+    ],
+    "github:jspm/nodelibs-process@0.1.1": [
+      "github:jspm/nodelibs-process@0.1.1/index"
+    ],
+    "scripts/model": [
+      "github:components/jquery@2.1.4",
+      "scripts/manager"
+    ],
+    "npm:asap@2.0.3/browser-raw": [
+      "github:jspm/nodelibs-process@0.1.1"
+    ],
+    "albums/js/models/photo": [
+      "scripts/model"
+    ],
+    "npm:asap@2.0.3/asap": [
+      "npm:asap@2.0.3/browser-raw",
+      "github:jspm/nodelibs-process@0.1.1"
+    ],
+    "npm:asap@2.0.3": [
+      "npm:asap@2.0.3/asap"
+    ],
+    "npm:q@2.0.3/q": [
+      "npm:weak-map@1.0.5",
+      "npm:pop-iterate@1.0.1",
+      "npm:asap@2.0.3",
+      "github:jspm/nodelibs-process@0.1.1"
+    ],
+    "npm:q@2.0.3": [
+      "npm:q@2.0.3/q"
+    ],
+    "general/js/hbs-pony": [
+      "github:components/jquery@2.1.4",
+      "github:components/handlebars.js@2.0.0",
+      "npm:q@2.0.3"
+    ],
+    "albums/js/album": [
+      "github:twbs/bootstrap@3.3.5",
+      "general/js/hbs-pony",
+      "albums/js/models/photo"
+    ],
+    "albums/js/photo-detail": [
+      "github:twbs/bootstrap@3.3.5"
+    ]
+  }
+});
+
+System.config({
   "map": {
     "babel": "npm:babel-core@5.6.4",
     "babel-runtime": "npm:babel-runtime@5.6.4",
