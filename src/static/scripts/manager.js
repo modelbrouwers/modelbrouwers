feb9eaef5fd5340c8d1a7eaae27e5218580fc265
Sergei Maertens <sergeimaertens@gmail.com>
2015-06-25 22:24:30 +0200

    Added manager.js

---
+++ src/static/scripts/manager.js
@@ -0,0 +1,79 @@
+import Q from 'q';
+import Api from 'scripts/api';
+import Paginator from 'scripts/paginator';
+
+
+class Manager {
+  constructor(modelClass) {
+    this.model = modelClass;
+    this._objectCache = {
+      _pages: {},
+      _objects: {}
+    };
+  }
+
+  _createObjs(raw_objects) {
+    var self = this;
+    var objs = raw_objects.map(function(props) {
+      var obj = new self.model(props);
+      self._objectCache._objects[obj.id] = obj;
+      return obj;
+    });
+    return objs;
+  }
+
+  all() {
+    var self = this;
+    var endpoint = self.model._meta.endpoints.list;
+
+    return Api.request(endpoint).get().then(function(response) {
+      var hasPagination = 'count' in response;
+      if (hasPagination) {
+        debugger; // TODO
+      } else { // response is a list of objects, so instatiate
+        return self._createObjs(response);
+      }
+    });
+  }
+
+  filter(filters) {
+    // TODO: block until promise is resolved and return the result immediately?
+    var endpoint = this.model._meta.endpoints.list;
+    var self = this;
+    return Api.request(endpoint, filters).get().then(function(response) {
+      var paginator = new Paginator({obj_creator: self._createObjs});
+      paginator.paginate(response, filters.page);
+      var objects = self._createObjs(response.results);
+
+      objects.page_obj = paginator;
+      if (paginator.page) {
+        paginator.objects = objects;
+        self._objectCache._pages[paginator.number] = paginator;
+      }
+      return objects;
+    });
+  }
+
+  get(filters) {
+    var self = this;
+    var endpoint = self.model._meta.endpoints.detail;
+    if ('id' in filters) {
+      // first check the local object cache
+      var _obj = self._objectCache._objects[filters.id];
+      if (_obj !== undefined) {
+        var deferred = Q.defer();
+        deferred.resolve(_obj);
+        return deferred.promise;
+      }
+      endpoint = endpoint.replace(':id', filters.id);
+      delete filters.id;
+    }
+    return Api.request(endpoint, filters).get().then(function(response) {
+      var objs = self._createObjs([response]);
+      return objs[0];
+    });
+  }
+
+}
+
+export default Manager;
