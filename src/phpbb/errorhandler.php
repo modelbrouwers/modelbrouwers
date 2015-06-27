b0989e549739eb710f7b48956ec0e23c925f4def
Sergei Maertens <sergeimaertens@gmail.com>
2015-06-27 22:36:17 +0200

    Added Raven error handler

---
+++ src/phpbb/errorhandler.php
@@ -0,0 +1,15 @@
+<?php
+
+define('DSN', $settings->RAVEN_DSN);
+
+function registerErrorhandler() {
+    $client = new Raven_Client(DSN);
+    $error_handler = new Raven_ErrorHandler($client);
+    $error_handler->registerExceptionHandler();
+    $error_handler->registerErrorHandler();
+    $error_handler->registerShutdownFunction();
+}
+
+registerErrorhandler();
+
+?>
