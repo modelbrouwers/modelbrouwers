6060f598620572a875fd9f51ab4544fd2de3be73
Sergei Maertens <sergeimaertens@gmail.com>
2015-07-14 09:23:31 +0200

    Added management command to generate all thumbs

---
+++ src/brouwers/albums/management/commands/generate_thumbs.py
@@ -0,0 +1,19 @@
+from django.core.management.base import NoArgsCommand
+
+from sorl.thumbnail import get_thumbnail
+
+
+class Command(NoArgsCommand):
+
+    sizes = (
+        '300x225',
+        '1280x1280',
+        '1024x1024',
+    )
+
+    def handle_noargs(self, *args, **options):
+        from brouwers.albums.models import Photo
+
+        for photo in Photo.objects.all():
+            for size in self.sizes:
+                get_thumbnail(photo.image, size)
