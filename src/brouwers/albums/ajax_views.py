1c8901d2186dbcfc511f82dbb1e1d44e37824e54
Sergei Maertens <sergeimaertens@gmail.com>
2015-07-15 19:52:59 +0200

    Fix some small bugs, remove old unused templates

--- src/brouwers/albums/ajax_views.py
+++ src/brouwers/albums/ajax_views.py
@@ -1,55 +1,10 @@
 import json
-import urllib2
-from urlparse import urlparse
-
-from django.db.models import Max
-from django.core.files import File
-from django.core.files.temp import NamedTemporaryFile
 from django.http import HttpResponse
-from django.shortcuts import render
 from django.views.generic import View
 from django.views.generic.detail import SingleObjectMixin
 
-from brouwers.general.decorators import login_required_403
-from .models import Photo, Preferences
-from .forms import PickAlbumForm, UploadFromURLForm
-from .utils import resize, admin_mode, get_default_img_size
-
-
-# uploading images from urls
-@login_required_403
-def upload_url(request):
-    albumform = PickAlbumForm(request.user, request.POST)
-    urlform = UploadFromURLForm(request.POST)
-    if albumform.is_valid() and urlform.is_valid():
-        url = urlform.cleaned_data['url']
-        album = albumform.cleaned_data['album']
-        name = urlparse(url).path.split('/')[-1]
-
-        tmp_img = NamedTemporaryFile(delete=True)
-        tmp_img.write(urllib2.urlopen(url).read())
-        tmp_img.flush()
-
-        max_order = Photo.objects.filter(album=album).aggregate(Max('order'))['order__max'] or 0
-        path = 'albums/%s/%s/' % (request.user.id, album.id)
-
-        photo = Photo(user=request.user, album=album)
-        photo.image.save(name, File(tmp_img))
-        photo.image.open()
-
-        # get the resizing dimensions from the preferences #TODO this might move to utils in the future
-        preferences = Preferences.get_or_create(request.user)
-        resize_dimensions = get_default_img_size(preferences)
-        img_data = resize(photo.image, upload_to=path, sizes_data=[resize_dimensions], overwrite=True)
-
-        for data in img_data:
-            photo.width = data[1]
-            photo.height = data[2]
-            photo.order = max_order + 1
-            photo.save()
-            p_id = photo.id
-        return HttpResponse(p_id, content_type="text/plain")
-    return render(request, 'albums/uploadify_url.html', {'urlform': urlform})
+from .models import Photo
+from .utils import admin_mode
 
 
 # CLASS BASED VIEWS ###
