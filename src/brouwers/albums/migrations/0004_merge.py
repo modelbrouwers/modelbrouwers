e8c4de9864278b02d0ca9c827f58b2f7c2cb394a
Sergei Maertens <sergeimaertens@gmail.com>
2015-08-04 09:18:15 +0200

    Resolve migration mess

--- src/brouwers/albums/migrations/0004_merge.py
+++ src/brouwers/albums/migrations/0004_merge.py
@@ -7,9 +7,9 @@ from django.db import models, migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('albums', '0002_auto_20150608_0900'),
+        ('albums', '0003_auto_20150723_2233'),
+        ('albums', '0003_auto_20150722_0917'),
     ]
 
     operations = [
-        # deliberately empty, there was a merge conflict with the migrations
     ]
