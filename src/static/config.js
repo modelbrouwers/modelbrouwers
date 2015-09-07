35ec65935fe918eedacb39a3f4c8d011a6d5e3ab
Sergei Maertens <sergeimaertens@gmail.com>
2015-09-07 09:23:05 +0200

    Get jspm infrastructure correctly in place

--- src/static/config.js
+++ src/static/config.js
@@ -15,10 +15,10 @@ System.config({
 
   map: {
     "URIjs": "npm:URIjs@1.15.1",
-    "babel": "npm:babel-core@5.6.15",
-    "babel-runtime": "npm:babel-runtime@5.6.15",
+    "babel": "npm:babel-core@5.8.23",
+    "babel-runtime": "npm:babel-runtime@5.8.20",
     "bootstrap": "github:twbs/bootstrap@3.3.5",
-    "core-js": "npm:core-js@0.9.18",
+    "core-js": "npm:core-js@1.1.4",
     "css": "github:systemjs/plugin-css@0.1.13",
     "handlebars": "github:components/handlebars.js@2.0.0",
     "jquery": "github:components/jquery@2.1.4",
@@ -54,10 +54,10 @@ System.config({
       "domain": "github:jspm/nodelibs-domain@0.1.0",
       "process": "github:jspm/nodelibs-process@0.1.1"
     },
-    "npm:babel-runtime@5.6.15": {
+    "npm:babel-runtime@5.8.20": {
       "process": "github:jspm/nodelibs-process@0.1.1"
     },
-    "npm:core-js@0.9.18": {
+    "npm:core-js@1.1.4": {
       "fs": "github:jspm/nodelibs-fs@0.1.2",
       "process": "github:jspm/nodelibs-process@0.1.1",
       "systemjs-json": "github:systemjs/plugin-json@0.1.0"
