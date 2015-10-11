cab9d6162b61f8dc10a901227bd761e7e0da046e
Sergei Maertens <sergeimaertens@gmail.com>
2015-10-11 15:39:02 +0200

    Nuke that popup, broken

--- src/brouwers/forum_tools/static/scripts/mods.js
+++ src/brouwers/forum_tools/static/scripts/mods.js
@@ -15,28 +15,28 @@ var urlconf = urlconf || {};
 			}
 		});
 
-		$('#popup').dialog({
-			autoOpen: false,
-			draggable: true,
-			//dialogClass: "",
-			height: 300,
-			width: 400,
-			modal: false,
-			resizable: true,
-			title: "Online gebruikers",
-			buttons: {
-				Sluiten: function() {
-					$(this).dialog("close");
-				}
-			}
-		});
+		// $('#popup').dialog({
+		// 	autoOpen: false,
+		// 	draggable: true,
+		// 	//dialogClass: "",
+		// 	height: 300,
+		// 	width: 400,
+		// 	modal: false,
+		// 	resizable: true,
+		// 	title: "Online gebruikers",
+		// 	buttons: {
+		// 		Sluiten: function() {
+		// 			$(this).dialog("close");
+		// 		}
+		// 	}
+		// });
 
-		$.get(urlconf.ou.ous, function(response){
-			if (response != '0'){
-				$('#popup').html(response);
-				$('#popup').dialog('open');
-			}
-		});
+		// $.get(urlconf.ou.ous, function(response){
+		// 	if (response != '0'){
+		// 		$('#popup').html(response);
+		// 		$('#popup').dialog('open');
+		// 	}
+		// });
 
 		$.get('/forum_tools/mods/get_data/', function(json){
 			if (json.open_reports > 0){
