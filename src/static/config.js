73e67ef962d254abe927bb200e88227d81d6b009
Sergei Maertens <sergeimaertens@gmail.com>
2015-11-05 22:15:56 +0100

    Temporary workaround for JSPM issue w/r to Django integration

--- src/static/config.js
+++ src/static/config.js
@@ -8,8 +8,8 @@ System.config({
     ]
   },
   paths: {
-    "github:*": "src/static/jspm_packages/github/*",
-    "npm:*": "src/static/jspm_packages/npm/*"
+    "github:*": "jspm_packages/github/*",
+    "npm:*": "jspm_packages/npm/*"
   },
 
   map: {
