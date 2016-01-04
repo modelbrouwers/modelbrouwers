6ae800bef478b4fef17277a5c6770697c8ae1e93
Sergei Maertens <sergeimaertens@gmail.com>
2016-01-04 19:23:16 +0100

    Correctly show url photo previews

--- src/brouwers/builds/static/builds/js/builds.js
+++ src/brouwers/builds/static/builds/js/builds.js
@@ -3,11 +3,22 @@ import 'jquery';
 
 $(function() {
 
-	$('fieldset').on('change', '.formset-form input[type="url"]', previewUrl);
+	$('fieldset').on('change, keyup', '.formset-form input[type="url"]', previewUrl);
 
 });
 
 
 let previewUrl = function(event) {
-	debugger;
+    let url = $(this).val();
+    let $form = $(this).closest('.formset-form');
+    let $img = $form.find('img');
+    let $preview = $form.find('.preview');
+    $preview.addClass('hidden'); // always hide, only show when real urls work
+
+    let img = new Image();
+    img.onload = function() {
+        $form.find('img').attr('src', url);
+        $preview.removeClass('hidden');
+    };
+    img.src = url; // trigger loading
 };
