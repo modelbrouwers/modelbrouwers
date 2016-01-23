d81c403848c81335bd12acc7c74e118865188cb7
Sergei Maertens <sergeimaertens@gmail.com>
2016-01-23 09:15:05 +0100

    Bootstrap Build.update view

--- src/brouwers/builds/views.py
+++ src/brouwers/builds/views.py
@@ -5,7 +5,10 @@ from django.shortcuts import get_object_or_404
 from django.views.generic import DetailView, ListView, RedirectView
 from django.views.generic.detail import SingleObjectMixin
 
-from extra_views import CreateWithInlinesView, InlineFormSet, NamedFormsetsMixin
+from extra_views import (
+    CreateWithInlinesView, InlineFormSet, NamedFormsetsMixin,
+    UpdateWithInlinesView
+)
 
 from brouwers.general.models import UserProfile
 from brouwers.utils.views import LoginRequiredMixin
@@ -14,6 +17,9 @@ from .models import Build, BuildPhoto
 
 
 User = get_user_model()
+
+
+# TODO: search
 
 
 class IndexView(ListView):
@@ -93,84 +99,19 @@ class BuildCreateView(LoginRequiredMixin, NamedFormsetsMixin, CreateWithInlinesV
     inlines = [PhotoInline]
     inlines_names = ['photos']
 
-    def get_success_url(self):
-        return self.object.get_absolute_url()
-
     def forms_valid(self, form, inlines):
         form.instance.user = self.request.user
         return super(BuildCreateView, self).forms_valid(form, inlines)
 
 
-# class BuildUpdate(UpdateView):
-#     form_class = BuildForm
-#     template_name = 'builds/edit.html'
+class BuildUpdateView(LoginRequiredMixin, NamedFormsetsMixin, UpdateWithInlinesView):
+    model = Build
+    form_class = BuildForm
+    inlines = [PhotoInline]
+    inlines_names = ['photos']
 
-#     def get_queryset(self):
-#         if self.request.user.has_perms('builds.edit_build'):
-#             return Build.objects.all()
-#         return Build.objects.filter(user_id=self.request.user.id)
+    def get_queryset(self):
+        qs = super(BuildUpdateView, self).get_queryset()
+        return qs.filter(user=self.request.user)
 
-#     def formfield_for_dbfield(self, db_field, **kwargs):
-#         """ Callback function to limit the photos that can be selected. """
-#         request = kwargs.pop('request', None)
-#         return buildphoto_formfield_callback(db_field, request, **kwargs)
-
-#     def get_formset_class(self):
-#         ff_callback = partial(self.formfield_for_dbfield, request=self.request)
-#         return inlineformset_factory(Build, BuildPhoto,
-#                                     exclude = ('order',),
-#                                     extra=10, max_num=10,
-#                                     formfield_callback = ff_callback,
-#                                     can_delete = True
-#                                     )
-
-#     def get_success_url(self):
-#         """ Show the detail page """
-#         return self.object.get_absolute_url()
-
-#     def get_formset(self, **kwargs):
-#         """ Method to easily get the formset in different stages """
-#         BuildPhotoFormset = self.get_formset_class()
-#         return BuildPhotoFormset(**kwargs)
-
-#     def form_valid(self, form):
-#         self.object = form.save(commit=False)
-#         formset = self.get_formset(instance=self.object, data=self.request.POST)
-#         if formset.is_valid():
-#             self.object.save()
-#             formset.save()
-#             return redirect(self.get_success_url())
-#         else:
-#             return self.form_invalid(form, formset=formset)
-
-#     def form_invalid(self, form, formset=None):
-#         context = {'form': form}
-#         if formset is not None:
-#             context['photos_formset'] = formset
-#         else:
-#             formset = self.get_formset(instance=self.object, data=self.request.POST)
-#             # trigger validation
-#             formset.is_valid()
-#             context['photos_formset'] = formset
-#         return self.render_to_response(self.get_context_data(**context))
-
-#     def get_context_data(self, **context):
-
-#         context['builds'] = Build.objects.filter(
-#                                 user = self.request.user
-#                             ).select_related(
-#                                 'user', 'profile', 'brand'
-#                             ).order_by('-pk')[:20]
-
-#         context['searchform'] = SearchForm()
-
-#         if not context.get('photos_formset', False):
-#             context['photos_formset'] = self.get_formset(instance=self.object)
-
-#         # get the image urls for each photo
-#         photos_data = {}
-#         for photo in self.object.buildphoto_set.select_related('photo').exclude(photo_id=None):
-#             photos_data[photo.photo.id] = photo.image_url
-#             context['photo_urls'] = json.dumps(photos_data)
-
-#         return super(BuildUpdate, self).get_context_data(**context)
+    # TODO: object-level permissions?
