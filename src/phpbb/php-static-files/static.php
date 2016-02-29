e8d8da22368d393effbc62a28ba7e3b35d94b0e5
Sergei Maertens <sergeimaertens@gmail.com>
2016-02-29 19:44:22 +0100

    Initial work on ManifestStaticFilesStorage on php side

--- src/phpbb/php-static-files/static.php
+++ src/phpbb/php-static-files/static.php
@@ -6,23 +6,56 @@ require_once __DIR__ . DIRECTORY_SEPARATOR . 'cache.php';
 require_once $settings->COMPOSER_AUTOLOADER;
 require_once $settings->PROJECT_DIR . DIRECTORY_SEPARATOR . 'errorhandler.php';
 
-// initialize the cache
-// $cache = new StaticCache();
-// $cache->init();
 
-class FileSystemStorage
+class StaticFilesStorage
 {
+
+	protected $base_location = null;
+	protected $location = null;
+	protected $base_url = null;
+
 	public function __construct() {
 		global $settings;
-		$this->static_url = $settings->STATIC_URL;
-		$this->static_root = $settings->STATIC_ROOT;
+		$this->location = $this->base_location = $settings->STATIC_ROOT;
+		$this->base_url = $settings->STATIC_URL;
 	}
 
 	/**
 	 * Get the url to the static file, either from cache or calculate the hashed path.
 	 */
 	public function url($file) {
-		return $this->static_url . $file;
+		return $this->base_url . $file;
+	}
+}
+
+
+/**
+ * Utility class to inject in the FileSystemStorage class. This takes care of
+ * computing/retrieving hashes to files. It's a workaround for mixins...
+ */
+class HashUtility
+{
+	protected $storage = null;
+
+	public function __construct($storage) {
+		$this->storage = $storage;
+	}
+
+	protected function get_hashed_name($file) {
+		if($this->storage->DEBUG) return $file;
+		$abs_path = realpath($this->storage->get_location() . DIRECTORY_SEPARATOR . $file);
+
+		$pathinfo = pathinfo($file);
+		$dirname = $pathinfo['dirname'];
+		$filename = $pathinfo['filename'];
+		$extension = $pathinfo['extension'];
+
+		$md5 = md5_file($abs_path);
+		$hash = substr($md5, 0, 12);
+
+		$hashed_filename = $filename . '.' . $hash . '.' . $extension;
+		$result = $dirname . DIRECTORY_SEPARATOR. $hashed_filename;
+		return $result;
 	}
 }
 
@@ -35,10 +68,9 @@ class FileSystemStorage
  * Memcached. If the hashed file doesn't exist, return the unchanged URL.
  * Django updates the cached filenames as part of the collectstatic command.
  */
-class CachedFilesStorage extends FileSystemStorage
+class CachedFilesStorage extends StaticFilesStorage
 {
-	protected $static_root;
-	protected $static_url;
+	protected $base_url;
 	protected $cache_key_prefix;
 	protected $systemjs_output_dir;
 
@@ -52,31 +84,15 @@ class CachedFilesStorage extends FileSystemStorage
 		$this->cache = $cache;
 		$this->DEBUG = (bool) getenv('DEBUG');
 		$this->systemjs_output_dir = $settings->SYSTEMJS_OUTPUT_DIR;
+		$this->hash_utility = new HashUtility($this);
 	}
 
-	protected function get_static_root() {
-		return $this->static_root;
+	protected function get_location() {
+		return $this->location;
 	}
 
-	protected function get_static_url() {
-		return $this->static_url;
-	}
-
-	protected function get_hashed_name($file) {
-		if($this->DEBUG) return $file;
-		$abs_path = realpath($this->get_static_root() . DIRECTORY_SEPARATOR . $file);
-
-		$pathinfo = pathinfo($file);
-		$dirname = $pathinfo['dirname'];
-		$filename = $pathinfo['filename'];
-		$extension = $pathinfo['extension'];
-
-		$md5 = md5_file($abs_path);
-		$hash = substr($md5, 0, 12);
-
-		$hashed_filename = $filename . '.' . $hash . '.' . $extension;
-		$result = $dirname . DIRECTORY_SEPARATOR. $hashed_filename;
-		return $result;
+	protected function get_base_url() {
+		return $this->base_url;
 	}
 
 	/**
@@ -95,10 +111,10 @@ class CachedFilesStorage extends FileSystemStorage
 		$hashed_name = ($this->DEBUG) ? false : $this->cache->get($cache_key);
 
 		if($hashed_name === false) {
-			$hashed_name = $this->get_hashed_name($file);
+			$hashed_name = $this->hash_utility->get_hashed_name($file);
 			if(!$this->DEBUG) $this->cache->set($cache_key, $hashed_name);
 		}
-		return $this->get_static_url() . $hashed_name;
+		return $this->get_base_url() . $hashed_name;
 	}
 
 	protected function getBundleScripts($apps) {
@@ -139,7 +155,7 @@ class CombinedStaticFilesStorage extends CachedFilesStorage
 	protected $CACHE_DIR = 'PHP_CACHE';
 
 	protected function get_cache_dir() {
-		$dirname = $this->get_static_root() . DIRECTORY_SEPARATOR . $this->CACHE_DIR;
+		$dirname = $this->get_location() . DIRECTORY_SEPARATOR . $this->CACHE_DIR;
 		if (!is_dir($dirname)) {
 			mkdir($dirname);
 			chmod($dirname, 0755);
@@ -150,9 +166,9 @@ class CombinedStaticFilesStorage extends CachedFilesStorage
 	public function url($files, $ext='js') {
 
 		if ($this->DEBUG) {
-			$static_url = $this->get_static_url();
-			$urls = array_map(function($path) use ($static_url) {
-				return $static_url . $path;
+			$base_url = $this->get_base_url();
+			$urls = array_map(function($path) use ($base_url) {
+				return $base_url . $path;
 			}, $files);
 			if ($ext == 'js') {
 				$glue = "\"></script>\n<script src=\"";
@@ -160,13 +176,13 @@ class CombinedStaticFilesStorage extends CachedFilesStorage
 			}
 		}
 
-		$root = $this->get_static_root();
+		$root = $this->get_location();
 		$file_paths = array();
 		$_output = array();
 
 		// calculate all the file hashes to definitely use the latest file
 		foreach ($files as $file) {
-			$filename = $root . DIRECTORY_SEPARATOR . $this->get_hashed_name($file);
+			$filename = $root . DIRECTORY_SEPARATOR . $this->hash_utility->get_hashed_name($file);
 			$file_paths[] = $filename;
 		}
 		$md5_result = md5(implode("::", $file_paths));
@@ -185,7 +201,7 @@ class CombinedStaticFilesStorage extends CachedFilesStorage
 		}
 
 		$url = substr($dest_file, strlen($root)+1);
-		return $this->get_static_url().$url;
+		return $this->get_base_url().$url;
 	}
 
 	protected function getBundleScripts($apps) {
@@ -195,6 +211,45 @@ class CombinedStaticFilesStorage extends CachedFilesStorage
 		}
 		$combinedUrl = $this->url($filenames, $ext='js');
 		return "<script type=\"text/javascript\" src=\"{$combinedUrl}\"></script>";
+	}
+}
+
+
+class ManifestStaticFilesStorage extends StaticFilesStorage
+{
+
+	protected $manifest_name = 'staticfiles.json';
+	protected $hashed_files = null;
+
+	public function __construct() {
+		parent::__construct();
+		$this->hashed_files = $this->loadManifest();
+	}
+
+	protected function loadManifest() {
+		// speed up: cache this in memcached?
+		$content = $this->readManifest();
+		if ($content === null) {
+			return array();
+		}
+		$stored = json_decode($content, true);
+		$version = $stored['version'];
+		if ($version == '1.0') {
+			return $stored['paths'] ?: array();
+		}
+		throw new Exception("Couldn't load manifest - unknown version {$version}");
+	}
+
+	protected function readManifest() {
+		$path = $this->location . DIRECTORY_SEPARATOR . $this->manifest_name;
+		$content = file_get_contents($path);
+		return $content;
+	}
+
+	public function url($file) {
+		if ($this->DEBUG) return $this->base_url . $file;
+		$cache_name = $this->hashed_files[$file] ?: null;
+		return $this->base_url . $cache_name;
 	}
 }
 
