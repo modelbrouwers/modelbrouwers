76300777dd0559837d111b05967fb759e5bcb47a
Clarity-89 <homes89@ukr.net>
2016-04-04 21:40:19 +0300

    Update kitreview model

--- src/brouwers/kitreviews/admin.py
+++ src/brouwers/kitreviews/admin.py
@@ -1,12 +1,18 @@
 from django.contrib import admin
-from models import KitReview, KitReviewVote, KitReviewProperty
+from models import KitReview, KitReviewVote, KitReviewProperty, KitReviewPropertyRating
+
+
+class KitReviewPropertyRatingInline(admin.TabularInline):
+    model = KitReviewPropertyRating
+    list_display = ('rating',)
 
 
 @admin.register(KitReview)
 class KitReviewAdmin(admin.ModelAdmin):
-    list_display = ('model_kit', 'rating', 'reviewer', 'submitted_on', 'last_edited_on')
-    list_filter = ('rating', 'reviewer', 'submitted_on')
+    list_display = ('model_kit', 'reviewer', 'submitted_on', 'last_edited_on')
+    list_filter = ('reviewer', 'submitted_on')
     search_fields = ('model_kit__name', 'reviewer__username', 'brand__name')
+    inlines = [KitReviewPropertyRatingInline]
 
 
 @admin.register(KitReviewVote)
@@ -16,5 +22,5 @@ class KitReviewVoteAdmin(admin.ModelAdmin):
 
 @admin.register(KitReviewProperty)
 class KitReviewPropertyAdmin(admin.ModelAdmin):
-    list_display = ('name', 'rating')
+    list_display = ('name',)
     search_fields = ('name',)
--- src/brouwers/kitreviews/models.py
+++ src/brouwers/kitreviews/models.py
@@ -1,5 +1,4 @@
 from django.conf import settings
-from django.contrib.sites.models import Site
 from django.core.urlresolvers import reverse
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
@@ -8,11 +7,9 @@ from djchoices import DjangoChoices, ChoiceItem
 
 from brouwers.albums.models import Album
 from brouwers.general.utils import get_username
+from brouwers.forum_tools.fields import ForumToolsIDField
 
-RATING_BASE = 100  # store ratings relative to 100
-RATING_DISPLAY_BASE = 5
 DEFAULT_RATING = 50
-DEFAULT_DIFFICULTY = 3
 MAX_RATING = 100
 MIN_RATING = 0
 
@@ -26,14 +23,12 @@ class KitReview(models.Model):
         help_text=_('This is your review. You can use BBCode here.')
     )
     html = models.TextField(blank=True, help_text=u'raw_text with BBCode rendered as html')
-    prop = models.ManyToManyField('KitReviewProperty', blank=True, related_name='reviews')
-    rating = models.PositiveSmallIntegerField(_(u'rating'), default=DEFAULT_RATING,
-                                              validators=[MinValueValidator(MIN_RATING), MaxValueValidator(MAX_RATING)])
+    properties = models.ManyToManyField('KitReviewProperty', blank=True, through='KitReviewPropertyRating', related_name='+')
 
     # linking to extra information
     album = models.ForeignKey(Album, verbose_name=_('album'), blank=True, null=True)
-    topic_id = models.PositiveIntegerField(
-        _('topic'), blank=True,
+    topic_id = ForumToolsIDField(
+        _('topic'), type='topic', blank=True,
         null=True, help_text=_('ID of the topic on Modelbrouwers.')
     )
     external_topic_url = models.URLField(
@@ -72,29 +67,22 @@ class KitReview(models.Model):
     @property
     def topic_url(self):
         if self.topic_id:
-            domain = Site.objects.get_current().domain
-            topic_url = "%(http)s%(domain)s%(phpBB)s%(topic)s" % {
-                'http': 'http://',  # TODO: check for https or http
-                'domain': domain,
-                'phpBB': settings.PHPBB_URL,
-                'topic': '/viewtopic.php?t=%d' % self.topic_id
-            }
-            return topic_url
+            return self.topic.get_absolute_url()
         elif self.external_topic_url:
             return self.external_topic_url
         return None
 
 
-class KitReviewVote(models.Model):
-    """ Model holding the votes for kitreviews, showing the quality of the review """
+class KitReviewVoteType(DjangoChoices):
+    positive = ChoiceItem('+', label='+')
+    negative = ChoiceItem('-', label='-')
 
-    VOTE_TYPES = (
-        ('+', '+'),
-        ('-', '-'),
-    )
+
+class KitReviewVote(models.Model):
+    """ Model holding the votes for kit reviews, showing the quality of the review """
 
     kit_review = models.ForeignKey(KitReview)
-    vote = models.CharField(_('vote'), max_length=1, db_index=True)
+    vote = models.CharField(_('vote'), max_length=1, db_index=True, choices=KitReviewVoteType.choices)
     voter = models.ForeignKey(settings.AUTH_USER_MODEL)
 
     class Meta:
@@ -104,23 +92,32 @@ class KitReviewVote(models.Model):
 
     def __unicode__(self):
         return _(u"Vote for review by %(review_submitter)s") % {
-            'review_submitter': get_username(self.kit_review, field='reviewer')
+            'review_submitter': self.kit_review.reviewer.username
         }
 
 
 class KitReviewProperty(models.Model):
-    """ Model containing the properties of a review, which based on their score are divided into pros and cons"""
+    """ Model containing the properties of a review """
 
     name = models.CharField(_(u'name'), max_length=255)
-    rating = models.PositiveSmallIntegerField(_(u'rating'), default=DEFAULT_RATING,
-                                              validators=[MinValueValidator(MIN_RATING), MaxValueValidator(MAX_RATING)])
 
     class Meta:
         verbose_name = _(u'kit review property')
         verbose_name_plural = _(u'kit review properties')
 
     def __unicode__(self):
-        return _(u'%(name)s: %(rating)s') % {
-            'name': self.name,
-            'rating': self.rating
+        return _(u'%(name)s') % {
+            'name': self.name
         }
+
+
+class KitReviewPropertyRating(models.Model):
+    """ Represents properties for a kit review rated on a scale from MIN_RATING to MAX_RATING """
+    kit_review = models.ForeignKey('KitReview')
+    prop = models.ForeignKey('KitReviewProperty') # 'property' is a reserved word it seems, maybe come up with smth better
+    rating = models.PositiveSmallIntegerField(_(u'rating'), default=DEFAULT_RATING,
+                                              validators=[MinValueValidator(MIN_RATING), MaxValueValidator(MAX_RATING)])
+
+    class Meta:
+        verbose_name = _(u'kit review property rating')
+        verbose_name_plural = _(u'kit review property ratings')
