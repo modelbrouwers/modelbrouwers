141b6d0a23ae0d5f115d87c280e73a870d290282
Sergei Maertens <sergeimaertens@gmail.com>
2016-05-07 12:51:18 +0200

    Handle some small code cleanups, fix some template strings

--- src/brouwers/brouwersdag/admin.py
+++ src/brouwers/brouwersdag/admin.py
@@ -3,12 +3,14 @@ from django.contrib import admin
 from .models import Competition, ShowCasedModel, Brouwersdag, Exhibitor
 
 
+@admin.register(ShowCasedModel)
 class ShowCasedModelAdmin(admin.ModelAdmin):
     list_display = ('name', 'owner', 'scale', 'length', 'width', 'height', 'is_competitor', 'id')
-    list_filter = ('competition', 'is_competitor', 'is_paid', 'scale')
+    list_filter = ('competition', 'is_competitor', 'is_paid', 'brouwersdag', 'scale')
     search_fields = ('competition__name', 'name', 'owner__username')
 
 
+@admin.register(Competition)
 class CompetitionAdmin(admin.ModelAdmin):
     list_display = ('__unicode__', 'name', 'max_num_models', 'max_participants', 'is_current')
     list_editable = ('name', 'max_num_models', 'max_participants', 'is_current')
@@ -19,13 +21,9 @@ class ExhibitorInline(admin.TabularInline):
     model = Exhibitor
 
 
+@admin.register(Brouwersdag)
 class BrouwersdagAdmin(admin.ModelAdmin):
     list_display = ('name', 'date')
     list_editable = ('date',)
     search_fields = ('name',)
     inlines = [ExhibitorInline]
-
-
-admin.site.register(ShowCasedModel, ShowCasedModelAdmin)
-admin.site.register(Competition, CompetitionAdmin)
-admin.site.register(Brouwersdag, BrouwersdagAdmin)
