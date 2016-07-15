87b65862fd56e82d3c3d220fe25d79b42b12aa6b
Sergei Maertens <sergeimaertens@gmail.com>
2016-07-15 09:47:50 +0200

    hook up the kit select widget

--- src/brouwers/kitreviews/forms.py
+++ src/brouwers/kitreviews/forms.py
@@ -3,6 +3,7 @@ from django.utils.translation import ugettext_lazy as _
 from django.db.models import Q
 
 from brouwers.kits.models import Brand, ModelKit, Scale
+from brouwers.kits.widgets import ModelKitSelect
 from brouwers.utils.forms import AlwaysChangedModelForm
 from brouwers.utils.widgets import RangeInput
 
@@ -49,11 +50,13 @@ class FindModelKitForm(forms.Form):
 
 
 class KitReviewForm(forms.ModelForm):
-    model_kit = forms.ModelChoiceField(queryset=ModelKit.objects.all(), required=True)
 
     class Meta:
         model = KitReview
         fields = ['model_kit', 'raw_text', 'album', 'topic', 'external_topic_url', 'show_real_name']
+        widgets = {
+            'model_kit': ModelKitSelect,
+        }
 
     def __init__(self, *args, **kwargs):
         self.user = kwargs.pop('user', None)
