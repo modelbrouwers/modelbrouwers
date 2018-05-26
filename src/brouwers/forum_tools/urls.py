31a5efa6f11da7d33b83268b53a77f832df65a51
Sergei Maertens <sergeimaertens@gmail.com>
2018-05-26 14:00:51 +0200

    Add some minimal tests for views & fix a bug
    Django 1.11 upgrade removed the support for passing Context() objects
    to templates to render, and this was missed because there were no tests

--- src/brouwers/forum_tools/urls.py
+++ src/brouwers/forum_tools/urls.py
@@ -1,17 +1,17 @@
 from django.conf.urls import url
 
 from .views import (
-    get_build_report_forums, get_chat, get_mod_data, get_posting_level,
-    get_sharing_perms, get_sync_data
+    BuildReportForumsView, ChatView, ModDataView, SyncDataView,
+    get_posting_level, get_sharing_perms
 )
 
 # Everything is AJAX
 app_name = 'forum_tools'
 urlpatterns = [
-    url(r'^get_sync_data/$', get_sync_data),
-    url(r'^get_chat/$', get_chat),
+    url(r'^get_sync_data/$', SyncDataView.as_view(), name='get_sync_data'),
+    url(r'^get_chat/$', ChatView.as_view(), name='get_chat'),
     url(r'^get_post_perm/$', get_posting_level),
-    url(r'^get_build_report_forums/$', get_build_report_forums),
-    url(r'^mods/get_data/$', get_mod_data),
-    url(r'^mods/get_sharing_perms/$', get_sharing_perms),
+    url(r'^get_build_report_forums/$', BuildReportForumsView.as_view(), name='get_build_report_forums'),
+    url(r'^mods/get_data/$', ModDataView.as_view(), name='get_mod_data'),
+    url(r'^mods/get_sharing_perms/$', get_sharing_perms, name='get_sharing_perms'),
 ]
