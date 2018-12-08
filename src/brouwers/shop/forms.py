e0b831e2574a7f26d318486f248bf08d8b743561
Oleksandr Khomenko <homes89@ukr.net>
2018-12-08 15:20:50 +0200

    Add rating select input

--- src/brouwers/shop/forms.py
+++ src/brouwers/shop/forms.py
@@ -1,10 +1,12 @@
 from django import forms
 
-from brouwers.shop.models import ProductReview
+from brouwers.shop.models import ProductReview, MAX_RATING, MIN_RATING
+from brouwers.utils.widgets import StarRatingSelect
 
 
 class ProductReviewForm(forms.ModelForm):
     text = forms.CharField(widget=forms.Textarea())
+    rating = forms.ChoiceField(choices=[(n, n) for n in range(MIN_RATING, MAX_RATING + 1)], widget=StarRatingSelect())
 
     class Meta:
         model = ProductReview
