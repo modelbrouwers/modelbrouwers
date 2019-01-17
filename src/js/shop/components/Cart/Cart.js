9ebcc8c4065965a514e12817bcde5b4f81edb8ff
Oleksandr Khomenko <homes89@ukr.net>
2019-01-17 22:37:36 +0200

    Display cart

--- src/js/shop/components/Cart/Cart.js
+++ src/js/shop/components/Cart/Cart.js
@@ -3,10 +3,35 @@ import React, { Component } from "react";
 export default class Cart extends Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            loading: false,
+            cart: {
+                total: 0,
+                products: []
+            }
+        };
     }
 
     render() {
-        return <div>Cart placeholder</div>;
+        const { cart, loading } = this.state;
+        return (
+            <div className="cart__row">
+                <div className="cart__box">
+                    <i className="fa fa-shopping-basket cart__icon" />
+                    {loading ? (
+                        <p>Loading...</p>
+                    ) : (
+                        <div className="cart__info">
+                            <div className="cart__items">
+                                {cart.products.length} item(s)
+                            </div>
+                            <div className="cart__price">
+                                &euro; {cart.total.toFixed(2)}
+                            </div>
+                        </div>
+                    )}
+                </div>
+            </div>
+        );
     }
 }
