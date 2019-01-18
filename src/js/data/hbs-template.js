60ca2f1696be43ccc3b3374fea03d1c6100a20e9
Sergei Maertens <sergeimaertens@gmail.com>
2019-01-18 09:23:59 +0100

    :bug: Fixed album detail -> photo lightbox

--- src/js/data/hbs-template.js
+++ src/js/data/hbs-template.js
@@ -13,20 +13,23 @@ class Template extends CrudConsumerObject {}
 class TemplateConsumer extends CrudConsumer {
     constructor(endpoint=`${API_ROOT}templates/`, objectClass=Template) {
         super(endpoint, objectClass);
+
+        this.defaultHeaders['Accept'] = 'application/json';
     }
 
     loadTemplate(app, name) {
         const tplName = `${app}::${name}`;
-        if (Handlebars.templates[tplName] != null) {
-            const tpl = Handlebars.templates[tplName];
+
+        if (TEMPLATE_CACHE[tplName] != null) {
+            const tpl = TEMPLATE_CACHE[tplName];
             return Promise.resolve(tpl);
         }
 
         return this
             .get(`${app}/${name}/`)
-            .then(tpl => {
-                Handlebars.templates[tplName] = Handlebars.compile(tpl);
-                return Handlebars.templates[tplName];
+            .then(response => {
+                TEMPLATE_CACHE[tplName] = Handlebars.compile(response.template);
+                return TEMPLATE_CACHE[tplName];
             });
     }
 }
