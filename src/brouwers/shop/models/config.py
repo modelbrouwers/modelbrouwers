523b21df151cee50506b14dad3cb49865752396d
Sergei Maertens <sergeimaertens@gmail.com>
2019-05-12 19:37:11 +0200

    :rotating_light: Ran isort

--- src/brouwers/shop/models/config.py
+++ src/brouwers/shop/models/config.py
@@ -1,6 +1,6 @@
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
 from django.utils.encoding import python_2_unicode_compatible
+from django.utils.translation import ugettext_lazy as _
 
 from solo.models import SingletonModel
 
