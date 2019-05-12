33117d06accfed1e5865032eec3ba4227739de8a
Sergei Maertens <sergeimaertens@gmail.com>
2019-05-12 19:36:53 +0200

    :sparkles: Add Sisow configuration

--- src/brouwers/shop/urls.py
+++ src/brouwers/shop/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     url(r'^$', IndexView.as_view(), name='index'),
     url(r'^categories/(?P<slug>[-_\w]+)/', CategoryDetailView.as_view(), name='category-detail'),
     url(r'^products/(?P<slug>[-_\w]+)/', ProductDetailView.as_view(), name='product-detail'),
-    url(r'^cart/(?P<pk>\d+)/', CartDetailView.as_view(), name='cart-detail')
+    url(r'^cart/(?P<pk>\d+)/', CartDetailView.as_view(), name='cart-detail'),
 ]
