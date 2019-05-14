497153971575f5ec510f12ecdd0613a2b0969e7e
Oleksandr Khomenko <homes89@ukr.net>
2019-05-14 22:39:22 +0300

    Isort

--- src/brouwers/general/api/viewsets.py
+++ src/brouwers/general/api/viewsets.py
@@ -1,5 +1,6 @@
+from rest_framework import status, views
 from rest_framework.response import Response
-from rest_framework import views, status
+
 from ..models import UserProfile
 from .serializers import UserProfileSerializer
 
