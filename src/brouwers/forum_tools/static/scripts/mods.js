51aea814416e12b95f3ab1ed42b294c991527fc4
Sergei Maertens <sergeimaertens@gmail.com>
2020-05-09 14:50:23 +0200

    :fire: Deleting more dead crap

--- src/brouwers/forum_tools/static/scripts/mods.js
+++ src/brouwers/forum_tools/static/scripts/mods.js
@@ -15,29 +15,6 @@ var urlconf = urlconf || {};
 			}
 		});
 
-		// $('#popup').dialog({
-		// 	autoOpen: false,
-		// 	draggable: true,
-		// 	//dialogClass: "",
-		// 	height: 300,
-		// 	width: 400,
-		// 	modal: false,
-		// 	resizable: true,
-		// 	title: "Online gebruikers",
-		// 	buttons: {
-		// 		Sluiten: function() {
-		// 			$(this).dialog("close");
-		// 		}
-		// 	}
-		// });
-
-		// $.get(urlconf.ou.ous, function(response){
-		// 	if (response != '0'){
-		// 		$('#popup').html(response);
-		// 		$('#popup').dialog('open');
-		// 	}
-		// });
-
 		$.get('/forum_tools/mods/get_data/', function(json){
 			if (json.open_reports > 0){
 				html = '&nbsp;<span id=\"open_reports\">('+json.text_reports+')</span>';
