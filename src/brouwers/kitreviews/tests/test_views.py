3db753c7024bcc73a85033c0c5b05135742f78eb
Sergei Maertens <sergeimaertens@gmail.com>
2020-11-19 23:14:47 +0100

    :green_heart: Fix tests because of newly introduced form element in modal markup

--- src/brouwers/kitreviews/tests/test_views.py
+++ src/brouwers/kitreviews/tests/test_views.py
@@ -17,10 +17,9 @@ from .factories import KitReviewFactory, KitReviewPropertyFactory
 
 
 class IndexViewTests(WebTest):
-
     def setUp(self):
         super().setUp()
-        self.url = reverse('kitreviews:index')
+        self.url = reverse("kitreviews:index")
 
     def test_index_shows_latest_reviews(self):
         """
@@ -31,7 +30,7 @@ class IndexViewTests(WebTest):
         index = self.app.get(self.url)
         expected = [repr(review) for review in kitreviews[5:]]
         expected.reverse()
-        self.assertQuerysetEqual(index.context['reviews'], expected)
+        self.assertQuerysetEqual(index.context["reviews"], expected)
 
 
 class AddReviewViewTests(WebTestFormMixin, LoginRequiredMixin, WebTest):
@@ -41,7 +40,7 @@ class AddReviewViewTests(WebTestFormMixin, LoginRequiredMixin, WebTest):
 
     def setUp(self):
         super().setUp()
-        self.url = reverse('kitreviews:add_review')
+        self.url = reverse("kitreviews:add_review")
 
     def test_submit_review(self):
         """
@@ -60,48 +59,56 @@ class AddReviewViewTests(WebTestFormMixin, LoginRequiredMixin, WebTest):
 
         self.assertFalse(KitReview.objects.exists())
         add_page = self.app.get(self.url, user=user)
-        form = add_page.form
+        form = add_page.forms[0]
 
         # try some invalid input
-        self.assertNotIn('model_kit', form.fields)  # no kit submitted - a review must belong to a kit
+        self.assertNotIn(
+            "model_kit", form.fields
+        )  # no kit submitted - a review must belong to a kit
         with self.assertRaises(ValueError):
-            form['album'].select(str(random.choice(invalid_albums).pk))  # an album from a different user
-        form['raw_text'] = ''  # empty review
+            form["album"].select(
+                str(random.choice(invalid_albums).pk)
+            )  # an album from a different user
+        form["raw_text"] = ""  # empty review
 
         response = form.submit()
-        self.assertFormError(response, 'form', 'raw_text', _('This field is required.'))
-        self.assertFormError(response, 'form', 'model_kit', _('This field is required.'))
+        self.assertFormError(response, "form", "raw_text", _("This field is required."))
+        self.assertFormError(
+            response, "form", "model_kit", _("This field is required.")
+        )
 
         # now select a kit
         kit = random.choice(kits)
-        self._add_field(form, 'model_kit', str(kit.pk))
+        self._add_field(form, "model_kit", str(kit.pk))
         # and enter a review text
-        form['raw_text'] = 'My very short review'
+        form["raw_text"] = "My very short review"
 
         response = form.submit()
         review = KitReview.objects.get()
         self.assertRedirects(response, review.get_absolute_url())
         self.assertEqual(review.ratings.count(), 3)
         self.assertEqual(review.model_kit, kit)
-        self.assertEqual(review.raw_text, 'My very short review')
+        self.assertEqual(review.raw_text, "My very short review")
         self.assertEqual(review.reviewer, user)
 
     def test_submit_review_for_kit(self):
         kit = ModelKitFactory.create()
         user = UserFactory.create()
-        url = reverse('kitreviews:review-add', kwargs={'slug': kit.slug})
+        url = reverse("kitreviews:review-add", kwargs={"slug": kit.slug})
         add_page = self.app.get(url, user=user)
-        form = add_page.form
+        form = add_page.forms[0]
         self._add_field(form, "model_kit", kit.pk)
-        form['raw_text'] = 'My review with [b]BBCode[/b]\nFoo'
+        form["raw_text"] = "My review with [b]BBCode[/b]\nFoo"
 
         response = form.submit()
 
         review = KitReview.objects.get()
         self.assertRedirects(response, review.get_absolute_url())
         self.assertEqual(review.model_kit, kit)
-        self.assertEqual(review.raw_text, 'My review with [b]BBCode[/b]\nFoo')
-        self.assertHTMLEqual(review.render_raw_text(), 'My review with <strong>BBCode</strong><br>Foo')
+        self.assertEqual(review.raw_text, "My review with [b]BBCode[/b]\nFoo")
+        self.assertHTMLEqual(
+            review.render_raw_text(), "My review with <strong>BBCode</strong><br>Foo"
+        )
         self.assertEqual(review.reviewer, user)
 
     def test_submit_review_with_topic(self):
@@ -117,36 +124,39 @@ class AddReviewViewTests(WebTestFormMixin, LoginRequiredMixin, WebTest):
         topic1 = TopicFactory.create()
         topic2 = TopicFactory.create(author=forum_user)
 
-        url = reverse('kitreviews:review-add', kwargs={'slug': kit.slug})
+        url = reverse("kitreviews:review-add", kwargs={"slug": kit.slug})
         add_page = self.app.get(url, user=user)
 
-        # non-authored topics may not be shown
-        with self.assertRaisesRegexp(ValueError, r'^Option %s not found \(from' % topic1.pk):
-            add_page.form['topic'].select(topic1.pk)
-        add_page.form['topic'].select(topic2.pk)
-        add_page.form['raw_text'] = 'Dummy review'
-        self._add_field(add_page.form, "model_kit", kit.pk)
+        form = add_page.forms[0]
 
-        add_page.form.submit().follow()
+        # non-authored topics may not be shown
+        with self.assertRaisesRegexp(
+            ValueError, r"^Option %s not found \(from" % topic1.pk
+        ):
+            form["topic"].select(topic1.pk)
+        form["topic"].select(topic2.pk)
+        form["raw_text"] = "Dummy review"
+        self._add_field(form, "model_kit", kit.pk)
+
+        form.submit().follow()
 
         review = KitReview.objects.get()
         self.assertEqual(review.topic, topic2)
 
 
 class SearchViewTests(WebTest):
-
     @classmethod
     def setUpTestData(cls):
         super().setUpTestData()
-        cls.url = reverse('kitreviews:find_kit')
+        cls.url = reverse("kitreviews:find_kit")
 
-        cls.kit1 = ModelKitFactory.create(name='Suzuki Katana')
-        cls.kit2 = ModelKitFactory.create(name='MiG-17F')
-        cls.kit3 = ModelKitFactory.create(name='Challenger Mk. IV', kit_number='1234')
+        cls.kit1 = ModelKitFactory.create(name="Suzuki Katana")
+        cls.kit2 = ModelKitFactory.create(name="MiG-17F")
+        cls.kit3 = ModelKitFactory.create(name="Challenger Mk. IV", kit_number="1234")
 
         cls.reviews = [
             KitReviewFactory.create(model_kit=cls.kit1),
-            KitReviewFactory.create(model_kit=cls.kit2)
+            KitReviewFactory.create(model_kit=cls.kit2),
         ]
 
     def test_search_form(self):
@@ -154,57 +164,62 @@ class SearchViewTests(WebTest):
         Test that the search form works as expected.
         """
 
-        # search by brand
-        search_page = self.app.get(self.url)
-        search_page.form['brand'].select(self.kit1.brand.pk)
-        search_results = search_page.form.submit()
-        queryset = search_results.context['kits']
-        self.assertQuerysetEqual(queryset, [repr(self.kit1)])
-        self.assertEqual(queryset[0].num_reviews, 1)
+        with self.subTest(search_by="brand"):
+            search_page = self.app.get(self.url)
+            form = search_page.forms[0]
+            form["brand"].select(self.kit1.brand.pk)
+            search_results = form.submit()
+            queryset = search_results.context["kits"]
+            self.assertQuerysetEqual(queryset, [repr(self.kit1)])
+            self.assertEqual(queryset[0].num_reviews, 1)
 
-        # search by scale
-        search_page = self.app.get(self.url)
-        search_page.form['scale'].select(self.kit2.scale.pk)
-        search_results = search_page.form.submit()
-        queryset = search_results.context['kits']
-        self.assertQuerysetEqual(queryset, [repr(self.kit2)])
-        self.assertEqual(queryset[0].num_reviews, 1)
+        with self.subTest(search_by="scale"):
+            search_page = self.app.get(self.url)
+            form = search_page.forms[0]
+            form["scale"].select(self.kit2.scale.pk)
+            search_results = form.submit()
+            queryset = search_results.context["kits"]
+            self.assertQuerysetEqual(queryset, [repr(self.kit2)])
+            self.assertEqual(queryset[0].num_reviews, 1)
 
-        # search by name
-        search_page = self.app.get(self.url)
-        search_page.form['kit_name'] = 'challenger'
-        search_results = search_page.form.submit()
-        self.assertQuerysetEqual(search_results.context['kits'], [repr(self.kit3)])
+        with self.subTest(search_by="name"):
+            search_page = self.app.get(self.url)
+            form = search_page.forms[0]
+            form["kit_name"] = "challenger"
+            search_results = form.submit()
+            self.assertQuerysetEqual(search_results.context["kits"], [repr(self.kit3)])
 
-        # search by name 2
-        search_page = self.app.get(self.url)
-        search_page.form['kit_name'] = 'katana'
-        search_results = search_page.form.submit()
-        self.assertQuerysetEqual(search_results.context['kits'], [repr(self.kit1)])
+        with self.subTest(search_by="name 2"):
+            search_page = self.app.get(self.url)
+            form = search_page.forms[0]
+            form["kit_name"] = "katana"
+            search_results = form.submit()
+            self.assertQuerysetEqual(search_results.context["kits"], [repr(self.kit1)])
 
-        # search by kit_number
-        search_page = self.app.get(self.url)
-        search_page.form['kit_number'] = '1234'
-        search_results = search_page.form.submit()
-        self.assertQuerysetEqual(search_results.context['kits'], [repr(self.kit3)])
+        with self.subTest(search_by="kit_number"):
+            search_page = self.app.get(self.url)
+            form = search_page.forms[0]
+            form["kit_number"] = "1234"
+            search_results = form.submit()
+            self.assertQuerysetEqual(search_results.context["kits"], [repr(self.kit3)])
 
     def test_invalid_search_form(self):
         """
         Test invalid search form input
         """
         # search by brand
-        max_pk = Brand.objects.order_by('pk').last().pk
-        response = self.client.get(self.url, {'brand': max_pk + 1})
-        self.assertIn('brand', response.context['form'].errors)
-        self.assertNotIn('kits', response.context)
+        max_pk = Brand.objects.order_by("pk").last().pk
+        response = self.client.get(self.url, {"brand": max_pk + 1})
+        self.assertIn("brand", response.context["form"].errors)
+        self.assertNotIn("kits", response.context)
 
     def test_posting_works(self):
         """
         Not intended, but it should work too
         """
         # search by brand
-        response = self.client.post(self.url, {'brand': self.kit1.brand.pk})
-        queryset = response.context['kits']
+        response = self.client.post(self.url, {"brand": self.kit1.brand.pk})
+        queryset = response.context["kits"]
         self.assertQuerysetEqual(queryset, [repr(self.kit1)])
         self.assertEqual(queryset[0].num_reviews, 1)
 
@@ -215,28 +230,28 @@ class SearchViewTests(WebTest):
         """
         # search by brand
         search_page = self.app.get(self.url)
-        search_page.form['kit_name'] = 'gibberish'
-        search_results = search_page.form.submit()
-        queryset = search_results.context['kits']
+        form = search_page.forms[0]
+        form["kit_name"] = "gibberish"
+        search_results = form.submit()
+        queryset = search_results.context["kits"]
         self.assertFalse(queryset.exists())
 
         search_url = search_results.request.url
 
-        self.assertEqual(len(search_results.forms), 2)
+        self.assertEqual(len(search_results.forms), 3)
         login_form = search_results.forms[1]
-        self.assertEqual(login_form.action, reverse('users:login'))
+        self.assertEqual(login_form.action, reverse("users:login"))
         login_page = login_form.submit()
 
-        user = UserFactory.create(password='letmein')
-        login_page.form['username'] = user.username
-        login_page.form['password'] = 'letmein'
+        user = UserFactory.create(password="letmein")
+        login_page.form["username"] = user.username
+        login_page.form["password"] = "letmein"
         search_results = login_page.form.submit().follow()
         self.assertEqual(search_results.request.url, search_url)
-        self.assertContains(search_results, _('Add kit'))
+        self.assertContains(search_results, _("Add kit"))
 
 
 class KitReviewsListViewTests(WebTest):
-
     def setUp(self):
         super().setUp()
 
@@ -245,11 +260,11 @@ class KitReviewsListViewTests(WebTest):
         self.kit2 = ModelKitFactory.create()
         self.reviews2 = KitReviewFactory.create_batch(1, model_kit=self.kit2)
 
-        self.url = reverse('kitreviews:review-list', kwargs={'slug': self.kit1.slug})
+        self.url = reverse("kitreviews:review-list", kwargs={"slug": self.kit1.slug})
 
     def test_correct_reviews_list(self):
         kit_detail = self.app.get(self.url)
-        reviews = kit_detail.context['object_list']
+        reviews = kit_detail.context["object_list"]
         expected_reviews = [repr(x) for x in self.reviews1]
         # TODO: ordering comes later - we'll order by review votes
         self.assertQuerysetEqual(reviews, expected_reviews, ordered=False)
@@ -262,19 +277,19 @@ class LegacyRedirectViewTests(WebTest):
     """
 
     def test_redirect(self):
-        url = '/kitreviews/kitreview_search_result_review.php'
+        url = "/kitreviews/kitreview_search_result_review.php"
         review = KitReviewFactory.create(legacy_id=42)
 
         # test success
-        response = self.app.get(url, params={'review': '42'})
+        response = self.app.get(url, params={"review": "42"})
         self.assertRedirects(response, review.get_absolute_url(), status_code=301)
 
-        response = self.app.get(url, params={'review': 42, 'kit': 'abcdefgh'})
+        response = self.app.get(url, params={"review": 42, "kit": "abcdefgh"})
         self.assertRedirects(response, review.get_absolute_url(), status_code=301)
 
         # test error handling
-        self.app.get(url, params={'review': 'abcdefgh'}, status=404)
+        self.app.get(url, params={"review": "abcdefgh"}, status=404)
         self.app.get(url, status=404)
 
         # test valid input, but unknown kit
-        response = self.app.get(url, params={'review': 41}, status=404)
+        response = self.app.get(url, params={"review": 41}, status=404)
