c071738bc8c0a2c58b7f499c4b9643f6cc7436f7
Sergei Maertens <sergeimaertens@gmail.com>
2020-11-28 17:42:21 +0100

    :whale: Include more assets in the phpbb container image

--- src/js/data/topic.js
+++ src/js/data/topic.js
@@ -6,7 +6,7 @@ class Topic extends CrudConsumerObject {}
 
 class TopicConsumer extends CrudConsumer {
     constructor(
-        endpoint = `${API_ROOT}forum_tools/topic/`,
+        endpoint = `${API_ROOT}api/v1/forum_tools/topic/`,
         objectClass = Topic
     ) {
         super(endpoint, objectClass);
