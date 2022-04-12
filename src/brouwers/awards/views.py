3e6053fa17b178acc3f808c87d027875af2c5f30
Sergei Maertens <sergeimaertens@gmail.com>
2022-04-13 00:16:14 +0200

    :sparkles: Redo hall of fame in bootstrap
    Bulk of the refactoring done, now just have to make it pretty.

--- src/brouwers/awards/views.py
+++ src/brouwers/awards/views.py
@@ -1,76 +1,66 @@
-from collections import OrderedDict
-from datetime import date
+from itertools import groupby
+from operator import attrgetter
+from typing import Optional
 
+from django.db.models import F, Window
+from django.db.models.functions import DenseRank
 from django.views.generic import TemplateView
 
 from .models import Project
-from .utils import voting_enabled as _voting_enabled
+
+
+def get_winners(year: int):
+    nominations = (
+        Project.objects.filter(nomination_date__year=year, rejected=False)
+        .select_related("category")
+        .annotate(
+            rank=Window(
+                expression=DenseRank(),
+                partition_by=[F("category")],
+                order_by=F("votes").desc(),
+            )
+        )
+        .order_by("category", "rank")
+    )
+
+    top_three_per_category = {}
+
+    for category, category_projects in groupby(nominations, key=attrgetter("category")):
+        top_three_per_category[category] = {1: [], 2: [], 3: []}
+        for project in category_projects:
+            if project.rank > 3:  # we only care about top three
+                break
+
+            top_three_per_category[category][project.rank].append(project)
+
+    return top_three_per_category
 
 
 class WinnersView(TemplateView):
 
     template_name = "awards/winners.html"
 
-    def get_context_data(self, **kwargs):
+    def get_context_data(self, year: Optional[int] = None, **kwargs):
         context = super().get_context_data(**kwargs)
-        requested_year = self.kwargs.get("year", None)
-        voting_enabled = _voting_enabled()
 
-        this_year = date.today().year
-        if requested_year is not None:
-            year = int(requested_year)
-        else:
-            year = this_year - 1
-            if voting_enabled:
-                year -= 1
-
-        context["year"] = year
-
-        # get the winners per category
-        if (
-            not voting_enabled
-            or (voting_enabled and year < this_year - 1)
-            or self.request.user.is_superuser
-        ):
-            context["winners_data"] = self.get_winners(year)
-
-        # list of years
-        context["years"] = (
+        editions = (
             Project.objects.exclude(rejected=True)
             .filter(votes__gt=0)
             .dates("nomination_date", "year", order="DESC")
         )
-        return context
 
-    def get_winners(self, year):
-        winners_data, prev_nomination = OrderedDict(), None
-        nominations = (
-            Project.objects.filter(nomination_date__year=year, rejected=False)
-            .select_related("category")
-            .order_by("category", "-votes")
+        # no year provided -> pick the most recent
+        if year is None and editions:
+            year = editions[0].year
+
+        top_three_per_category = get_winners(year) if year else {}
+
+        context.update(
+            {
+                "editions": editions,
+                "year": year,
+                "top_three_per_category": top_three_per_category,
+            }
         )
 
-        for nomination in nominations:
-            category = nomination.category
-
-            if (
-                prev_nomination
-                and winners_data.get(category, False)
-                and nomination.votes == prev_nomination.votes
-            ):
-                winners_data[category][position].append(nomination)
-            else:
-                if category not in winners_data:
-                    position, prev_nomination = "first", None
-                    winners_data[category] = OrderedDict()
-                else:
-
-                    if "second" not in winners_data[category]:
-                        position = "second"
-                    elif "third" not in winners_data[category]:
-                        position = "third"
-                    else:
-                        continue
-                winners_data[category][position] = [nomination]
-            prev_nomination = nomination
-        return winners_data
+        return context
