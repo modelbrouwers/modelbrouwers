c054e67218bb978ffe85a339b58fa5174ed0fa4f
Sergei Maertens <sergeimaertens@gmail.com>
2022-04-12 23:00:42 +0200

    :fire: Remove old templates and set up (temporary) redirects instead

--- src/brouwers/awards/urls.py
+++ src/brouwers/awards/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
-from django.views.generic import RedirectView, TemplateView
+from django.views.generic import RedirectView
 
 from .views import WinnersView
 
 app_name = "awards"
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="awards/landing.html"), name="index"),
+    path(
+        "",
+        RedirectView.as_view(pattern_name="awards:winners", permanent=False),
+        name="index",
+    ),
     path("hall-of-fame/", WinnersView.as_view(), name="winners"),
     path("hall-of-fame/<int:year>/", WinnersView.as_view(), name="winners"),
     # old redirect URLs, keep them for SEO
