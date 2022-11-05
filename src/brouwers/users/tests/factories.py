5548ee3a0b9d533d1f845ac75fa132ba1f0d7a76
Sergei Maertens <sergeimaertens@gmail.com>
2022-11-05 16:07:36 +0100

    :white_check_mark: [#144] -- add test for payment methods management in admin

--- src/brouwers/users/tests/factories.py
+++ src/brouwers/users/tests/factories.py
@@ -6,14 +6,20 @@ from ..models import User
 
 
 class UserFactory(factory.django.DjangoModelFactory):
-    class Meta:
-        model = User
-
     username = factory.Sequence(lambda n: "User {0}".format(n))
     password = factory.PostGenerationMethodCall("set_password", "password")
     email = factory.Sequence(lambda n: "user-{0}@gmail.com".format(n))
     is_active = True
     last_login = timezone.now()
+
+    class Meta:
+        model = User
+
+    class Params:
+        superuser = factory.Trait(
+            is_staff=True,
+            is_superuser=True,
+        )
 
 
 class DataDownloadRequestFactory(factory.django.DjangoModelFactory):
