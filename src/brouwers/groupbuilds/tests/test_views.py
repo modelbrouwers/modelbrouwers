572adb22b5229bb0ffab64fef50779ce202dd145
Sergei Maertens <sergeimaertens@gmail.com>
2024-03-04 22:23:31 +0100

    :fire: Delete obsolete code/tests for group builds

--- src/brouwers/groupbuilds/tests/test_views.py
+++ src/brouwers/groupbuilds/tests/test_views.py
@@ -1,26 +1,16 @@
 from django_webtest import WebTest
 
-from brouwers.users.tests.factories import UserFactory
-
+from ..constants import GroupbuildStatuses
 from .factories import GroupBuildFactory
 
 
 class ViewTests(WebTest):
     def test_detail_page(self):
-        gb = GroupBuildFactory.create()
-        gb.admins.add(gb.applicant)
+        gb = GroupBuildFactory.create(status=GroupbuildStatuses.accepted)
         url = gb.get_absolute_url()
 
         # anonymous user
         response = self.app.get(url)
+
         self.assertEqual(response.status_code, 200)
         self.assertIn(gb.theme, response)
-
-        # login as owner user
-        response = self.app.get(url, user=gb.applicant)
-        self.assertEqual(response.status_code, 200)
-
-        # and as superuser
-        superuser = UserFactory.create(is_staff=True, is_superuser=True)
-        response = self.app.get(url, user=superuser)
-        self.assertEqual(response.status_code, 200)
