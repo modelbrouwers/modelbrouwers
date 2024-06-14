80b4a98864802ce1177a6222cb9a055b710cf131
Sergei Maertens <sergeimaertens@gmail.com>
2024-06-14 22:46:08 +0200

    :white_check_mark: Add story for Address

---
+++ src/js/shop/components/Checkout/Address.stories.ts
@@ -0,0 +1,45 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { fn } from "@storybook/test";
+import {
+  reactRouterParameters,
+  withRouter,
+} from "storybook-addon-remix-react-router";
+
+import Address from "./Address";
+
+export default {
+  title: "Shop / Checkout / Address",
+  component: Address,
+  decorators: [withRouter],
+  args: {
+    customer: {
+      firstName: "Arsene",
+      lastName: "Lupin",
+      email: "arsene@lupin.fr",
+      phone: "",
+    },
+    deliveryAddress: {
+      company: "",
+      chamberOfCommerce: "",
+      street: "Avenue des Champs-Élysées",
+      number: "42",
+      city: "Paris",
+      postalCode: "75008",
+      country: "N",
+    },
+    billingAddress: undefined,
+    allowSubmit: false,
+    onChange: fn(),
+  },
+  parameters: {
+    reactRouter: reactRouterParameters({
+      routing: { path: "/winkel/checkout/address" },
+    }),
+  },
+} satisfies Meta<typeof Address>;
+
+type Story = StoryObj<typeof Address>;
+
+export const Default: Story = {
+  name: "Address",
+};
