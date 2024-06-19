05e2054b5f386cde03a9ee607f94ae0c00c51105
Sergei Maertens <sergeimaertens@gmail.com>
2024-06-19 22:18:08 +0200

    :recycle: Refactor country field to use base select

--- src/js/components/forms/CountryField.tsx
+++ src/js/components/forms/CountryField.tsx
@@ -1,17 +1,19 @@
-import clsx from "clsx";
-import { useField } from "formik";
-import { ReactNode, useId } from "react";
-import Select, { Props, GroupBase } from "react-select";
+import { ComponentProps, ReactNode } from "react";
 import { useIntl } from "react-intl";
 import countries from "i18n-iso-countries";
 
-import ErrorList from "./ErrorList";
-import FormGroup from "./FormGroup";
+import Select from "./Select";
 
 // TODO: can we split this in the bundle?
 countries.registerLocale(require("i18n-iso-countries/langs/en.json"));
 countries.registerLocale(require("i18n-iso-countries/langs/nl.json"));
 countries.registerLocale(require("i18n-iso-countries/langs/de.json"));
+
+const SUPPORTED_COUNTRIES = {
+  N: (lang: string) => countries.getName("NL", lang),
+  B: (lang: string) => countries.getName("BE", lang),
+  D: (lang: string) => countries.getName("DE", lang),
+};
 
 export interface CountryOption {
   value: "N" | "B" | "D";
@@ -23,21 +25,17 @@ export interface CountryFieldProps {
   label: ReactNode;
 }
 
-const SUPPORTED_COUNTRIES = {
-  N: (lang: string) => countries.getName("NL", lang),
-  B: (lang: string) => countries.getName("BE", lang),
-  D: (lang: string) => countries.getName("DE", lang),
-};
+type SelectProps = Omit<
+  ComponentProps<typeof Select<CountryOption>>,
+  "options"
+>;
 
-const CountryField: React.FC<
-  CountryFieldProps & Props<CountryOption, false, GroupBase<CountryOption>>
-> = ({ name, label, ...props }) => {
+const CountryField: React.FC<CountryFieldProps & SelectProps> = ({
+  name,
+  label,
+  ...props
+}) => {
   const intl = useIntl();
-  const id = useId();
-  const [{ value, ...field }, , helpers] = useField<
-    CountryOption["value"] | undefined
-  >(name);
-
   // build localized list of country names
   const langCode = intl.locale.split("-")[0];
   const countryOptions = Object.entries(SUPPORTED_COUNTRIES).map(
@@ -46,38 +44,13 @@ const CountryField: React.FC<
       label: nameGetter(langCode),
     }),
   );
-
-  const selectedOption = countryOptions.find(
-    (option) => option.value === value,
-  );
-
   return (
-    <FormGroup>
-      {label && (
-        <label
-          htmlFor={id}
-          className={clsx("control-label", { required: props.required })}
-        >
-          {label}
-        </label>
-      )}
-      <Select<CountryOption, false>
-        id={id}
-        options={countryOptions}
-        className=""
-        value={selectedOption}
-        {...field}
-        onChange={(option: CountryOption | null) => {
-          if (option === null) {
-            helpers.setValue(undefined);
-            return;
-          }
-          helpers.setValue(option.value);
-        }}
-        {...props}
-      />
-      <ErrorList name={name} />
-    </FormGroup>
+    <Select<CountryOption>
+      name={name}
+      label={label}
+      options={countryOptions}
+      {...props}
+    />
   );
 };
 
