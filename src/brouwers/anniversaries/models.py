86ff707379c7add1f74a3f0389c902e8ca718c38
Sergei Maertens <sergeimaertens@gmail.com>
2024-09-26 09:44:19 +0200

    :card_file_box: Add data model and admin registration for anniversary timeline events

---
+++ src/brouwers/anniversaries/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from ckeditor.fields import RichTextField
+
+
+class RemarkableEvent(models.Model):
+    date = models.DateField(
+        _("date"),
+        help_text=_("The day on which this significant event happened."),
+    )
+    title = models.CharField(
+        _("title"),
+        max_length=200,
+        help_text=_("Short title that should invite the user to read more."),
+    )
+    body_text = RichTextField(
+        _("body text"),
+        blank=True,
+        help_text=_("The full text providing more context about this specific moment."),
+    )
+    image = models.ImageField(
+        _("image"),
+        upload_to="anniversary/20/",
+        blank=True,
+        help_text=_("An image to display on the timeline."),
+    )
+
+    class Meta:
+        verbose_name = _("remarkable event")
+        verbose_name_plural = _("remarkable events")
+        ordering = ("date",)
+
+    def __str__(self) -> str:
+        return self.title
