671bbdd7509036201c9eb98de42ff0be4da9cfc9
Sergei Maertens <sergeimaertens@gmail.com>
2024-09-27 10:34:22 +0200

    :lock: Add superuser requirement for now

--- src/brouwers/anniversaries/views.py
+++ src/brouwers/anniversaries/views.py
@@ -1,9 +1,13 @@
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import ListView
 
 from .models import RemarkableEvent
 
 
-class TwentyYearsAnniversaryView(ListView):
+class TwentyYearsAnniversaryView(LoginRequiredMixin, UserPassesTestMixin, ListView):
     queryset = RemarkableEvent.objects.order_by("-date")
     template_name = "anniversaries/20.html"
     context_object_name = "events"
+
+    def test_func(self):
+        return self.request.user.is_superuser
