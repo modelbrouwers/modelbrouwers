ffe44d1213c177336a8e00d589a41099ed805cd8
Sergei Maertens <sergeimaertens@gmail.com>
2024-10-01 12:31:28 +0200

    :lock: Use Django model permission instead of only allowing superusers

--- src/brouwers/anniversaries/views.py
+++ src/brouwers/anniversaries/views.py
@@ -1,13 +1,11 @@
-from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.views.generic import ListView
 
 from .models import RemarkableEvent
 
 
-class TwentyYearsAnniversaryView(LoginRequiredMixin, UserPassesTestMixin, ListView):
+class TwentyYearsAnniversaryView(PermissionRequiredMixin, ListView):
     queryset = RemarkableEvent.objects.order_by("-date")
     template_name = "anniversaries/20.html"
     context_object_name = "events"
-
-    def test_func(self):
-        return self.request.user.is_superuser
+    permission_required = "anniversaries.add_remarkableevent"
