4ed6413d2bfb558594987cc3ae28de5845e6fb9b
Sergei Maertens <sergeimaertens@gmail.com>
2024-11-11 22:15:47 +0100

    :lock: Make 20-year anniversary page publicly accessible

--- src/brouwers/anniversaries/views.py
+++ src/brouwers/anniversaries/views.py
@@ -1,11 +1,9 @@
-from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.views.generic import ListView
 
 from .models import RemarkableEvent
 
 
-class TwentyYearsAnniversaryView(PermissionRequiredMixin, ListView):
+class TwentyYearsAnniversaryView(ListView):
     queryset = RemarkableEvent.objects.order_by("date")
     template_name = "anniversaries/20.html"
     context_object_name = "events"
-    permission_required = "anniversaries.add_remarkableevent"
