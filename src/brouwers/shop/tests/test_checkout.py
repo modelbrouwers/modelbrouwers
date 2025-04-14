0fbb5c4c09ed99b1775a6c776bc87d1c2ad1ac0b
Sergei Maertens <sergeimaertens@gmail.com>
2025-04-14 23:00:15 +0200

    :sparkles: [#155] Calculate order shipping costs based on delivery method
    Depending on the delivery method requested, calculate the appropriate
    shipping costs and store them on the order instance so that it's part
    of the order snapshot data.

--- src/brouwers/shop/tests/test_checkout.py
+++ src/brouwers/shop/tests/test_checkout.py
@@ -4,18 +4,21 @@ from unittest.mock import patch
 
 from django.core import mail
 from django.urls import reverse
+from django.utils.translation import gettext as _
 
 from django_webtest import WebTest
 
+from brouwers.general.constants import CountryChoices
 from brouwers.utils.tests.html_assert import strip_all_attributes
 
-from ..constants import CART_SESSION_KEY
+from ..constants import CART_SESSION_KEY, CartStatuses, DeliveryMethods
 from ..models import Cart, Order, ShopConfiguration
 from .factories import (
     CartProductFactory,
     CategoryFactory,
     PaymentMethodFactory,
     ProductFactory,
+    ShippingCostFactory,
 )
 
 
@@ -29,6 +32,13 @@ class CheckoutTests(WebTest):
 
         cls.payment_method = PaymentMethodFactory.create(
             name="Bank transfer", method="bank_transfer", enabled=True
+        )
+
+        ShippingCostFactory.create(
+            country=CountryChoices.nl, max_weight=100, price=Decimal("4.95")
+        )
+        ShippingCostFactory.create(
+            country=CountryChoices.nl, max_weight=1000, price=Decimal("9.95")
         )
 
     def _get_cart_id(self) -> int:
@@ -49,6 +59,7 @@ class CheckoutTests(WebTest):
             "last_name": "Chocolonely",
             "email": "tony@example.com",
             "phone": "",
+            "delivery_method": DeliveryMethods.mail,
             "delivery_address": {
                 "street": "Sesamstraat",
                 "number": "101",
@@ -70,7 +81,7 @@ class CheckoutTests(WebTest):
         errors = json.loads(response.pyquery("#checkout-errors").text())
         self.assertIn("cart", errors)
 
-    def test_checkout_sends_confirmation_email(self):
+    def test_checkout_with_products_and_delivery_via_email(self):
         # set up mocks
         mock_config = ShopConfiguration(
             bank_transfer_instructions="[bank transfer instructions here]",
@@ -93,17 +104,20 @@ class CheckoutTests(WebTest):
             price=Decimal("5.14"),
             vat=Decimal("0.20"),  # easier math in tests :-)
             categories=[category],
+            weight=100,  # grams
         )
         product2 = ProductFactory.create(
             name="Testproduct",
             price=Decimal("10.00"),
             vat=Decimal("0.20"),  # easier math in tests :-)
             categories=[category],
+            weight=30,  # grams
         )
         cart_id = self._get_cart_id()
         cart = Cart.objects.get(pk=cart_id)
         CartProductFactory.create(cart=cart, product=product1, amount=3)
         CartProductFactory.create(cart=cart, product=product2, amount=2)
+        assert cart.weight == 360
         checkout_data = {
             "cart": cart_id,
             "payment_method": self.payment_method.id,
@@ -112,6 +126,7 @@ class CheckoutTests(WebTest):
             "last_name": "Chocolonely",
             "email": "tony@example.com",
             "phone": "",
+            "delivery_method": DeliveryMethods.mail,
             "delivery_address": {
                 "street": "Sesamstraat",
                 "number": "101",
@@ -122,6 +137,18 @@ class CheckoutTests(WebTest):
                 "chamber_of_commerce": "",
             },
         }
+
+        # fetch expected/calculated shipping costs displayed in frontend to ensure
+        # both places calculate the same value
+        shipping_costs_response = self.app.get(
+            reverse("api:shipping-costs"),
+            params={
+                "cart_id": cart_id,
+                "country": checkout_data["delivery_address"]["country"],
+            },
+        )
+        assert shipping_costs_response.status_code == 200
+        expected_shipping_costs = Decimal(shipping_costs_response.json["price"])
 
         url = reverse("shop:confirm-checkout")
 
@@ -134,13 +161,17 @@ class CheckoutTests(WebTest):
 
         self.assertEqual(response.status_code, 302)
 
+        order = Order.objects.get()
+
+        with self.subTest("shipping costs endpoint sanity check"):
+            self.assertEqual(order.shipping_costs, expected_shipping_costs)
+
         with self.subTest("email sent"):
             self.assertEqual(len(mail.outbox), 1)
             message = mail.outbox[0]
             self.assertEqual(message.from_email, "shop@example.com")
 
-            order = Order.objects.get()
-            self.assertIn(order.reference, message.subject)
+            self.assertIn(order.reference, str(message.subject))
             self.assertEqual(message.extra_headers, {"Content-Language": "nl"})
 
             content_by_content_type = {
@@ -159,7 +190,67 @@ class CheckoutTests(WebTest):
                 self.assertInHTML("<td>&euro;&nbsp;10,00</td>", html_body)
                 self.assertInHTML("<td>&euro;&nbsp;20,00</td>", html_body)
 
+            with self.subTest("shipping costs"):
+                self.assertIn(_("Shipping costs"), html_body)
+                self.assertInHTML("<td>&euro;&nbsp;9,95</td>", html_body)
+
             with self.subTest("totals"):
                 # 0.20 * 3 * 5.14 + 0.20 * 2 * 10 -> rounded = 7.08
+                # 35.42 + 9.95 = 45.37 (products + shipping)
                 self.assertInHTML("<td>&euro;&nbsp;7,08</td>", html_body)
-                self.assertInHTML("<td>&euro;&nbsp;35,42</td>", html_body)
+                self.assertInHTML("<td>&euro;&nbsp;45,37</td>", html_body)
+
+    def test_checkout_delivery_by_mail_without_delivery_address_is_blocked(self):
+        self.addCleanup(ShopConfiguration.clear_cache)
+        # set up test data
+        cart_id = self._get_cart_id()
+        cart = Cart.objects.get(pk=cart_id)
+        CartProductFactory.create(cart=cart)
+        checkout_data = {
+            "cart": cart_id,
+            "payment_method": self.payment_method.id,
+            "payment_method_options": None,
+            "first_name": "Tony",
+            "last_name": "Chocolonely",
+            "email": "tony@example.com",
+            "phone": "",
+            "delivery_address": None,
+            "delivery_method": DeliveryMethods.mail,
+        }
+        url = reverse("shop:confirm-checkout")
+
+        response = self.app.post(url, {"checkoutData": json.dumps(checkout_data)})
+
+        self.assertEqual(response.status_code, 200)
+        errors = response.context["serializer"].errors
+        self.assertIn("delivery_address", errors)
+
+    def test_checkout_pickup_in_shop(self):
+        self.addCleanup(ShopConfiguration.clear_cache)
+        # set up test data
+        cart_id = self._get_cart_id()
+        cart = Cart.objects.get(pk=cart_id)
+        CartProductFactory.create(cart=cart)
+        checkout_data = {
+            "cart": cart_id,
+            "payment_method": self.payment_method.id,
+            "payment_method_options": None,
+            "first_name": "Tony",
+            "last_name": "Chocolonely",
+            "email": "tony@example.com",
+            "phone": "",
+            "delivery_address": None,
+            "delivery_method": DeliveryMethods.pickup,
+        }
+        url = reverse("shop:confirm-checkout")
+
+        response = self.app.post(url, {"checkoutData": json.dumps(checkout_data)})
+
+        self.assertEqual(response.status_code, 302)
+        cart.refresh_from_db()
+        self.assertEqual(cart.status, CartStatuses.processing)
+        order = Order.objects.get()
+        self.assertEqual(order.cart, cart)
+        self.assertEqual(order.shipping_costs, 0)
+        self.assertIsNone(order.delivery_address)
+        self.assertIsNone(order.invoice_address)
