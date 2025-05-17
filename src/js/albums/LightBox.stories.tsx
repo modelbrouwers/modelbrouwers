b1f7a8af6d5752bcc9e678601d5b83efe5967267
Sergei Maertens <sergeimaertens@gmail.com>
2025-05-17 21:13:34 +0200

    :white_check_mark: Add story for albums lightbox

---
+++ src/js/albums/LightBox.stories.tsx
@@ -0,0 +1,76 @@
+import type {Meta, StoryObj} from '@storybook/react';
+import {HttpResponse, http} from 'msw';
+
+import {API_ROOT} from '@/constants.js';
+
+import LightBox from './LightBox';
+
+export default {
+  title: 'Albums / LightBox',
+  component: LightBox,
+  decorators: [
+    Story => (
+      <>
+        <div className="modal fade lightbox in" style={{display: 'block', paddingRight: '15px'}}>
+          <div className="modal-dialog modal-lg">
+            <div className="modal-content">
+              <Story />
+            </div>
+          </div>
+        </div>
+        <div className="modal-backdrop fade in" />
+      </>
+    ),
+  ],
+  args: {
+    albumId: 99,
+    page: 1,
+    selectedPhotoId: 33,
+  },
+  parameters: {
+    msw: {
+      handlers: [
+        http.get(`${API_ROOT}api/v1/albums/photo/`, ({request}) =>
+          HttpResponse.json({
+            count: 20,
+            paginate_by: 2,
+            previous: null,
+            next: `${request.url}&page=2`,
+            results: [
+              {
+                id: 33,
+                user: {username: 'BBT'},
+                description: 'Image description',
+                image: {
+                  large: './static/images/thumb.png',
+                  thumb: './static/images/thumb.png',
+                },
+                width: 400,
+                height: 320,
+                uploaded: '2025-01-01T12:00:00+00:00',
+                order: 1,
+              },
+              {
+                id: 34,
+                user: {username: 'BBT'},
+                description: 'Image 2 description',
+                image: {
+                  large: './static/images/thumb.png',
+                  thumb: './static/images/thumb.png',
+                },
+                width: 400,
+                height: 320,
+                uploaded: '2025-01-01T12:00:04+00:00',
+                order: 2,
+              },
+            ],
+          }),
+        ),
+      ],
+    },
+  },
+} satisfies Meta<typeof LightBox>;
+
+type Story = StoryObj<typeof LightBox>;
+
+export const Default: Story = {name: 'LightBox'};
