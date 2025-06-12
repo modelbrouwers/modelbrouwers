1e3dde1160a81c07f09f251b6a7a26f84bd65e1f
Sergei Maertens <sergeimaertens@gmail.com>
2025-06-12 10:36:48 +0200

    :coffin: Delete dead user search code
    This is not used any longer. I think it existed for the group builds
    to have auto-complete on user names.

--- src/brouwers/general/ajax_views.py
+++ src/brouwers/general/ajax_views.py
@@ -1,39 +1,7 @@
-import json
-
-from django.db.models import Q
-from django.http import HttpResponse, JsonResponse
+from django.http import JsonResponse
 from django.views.generic.base import View
 
-from brouwers.general.decorators import login_required_403
-
-from .models import Announcement, UserProfile
-
-
-@login_required_403
-def search_users(request):
-    inputresults = request.GET.__getitem__("term").split(" ")
-    query = []
-    for value in inputresults:
-        q = (
-            Q(forum_nickname__icontains=value)
-            | Q(user__first_name__icontains=value)
-            | Q(user__last_name__icontains=value)
-        )
-        query.append(q)
-    if (
-        len(query) > 0 and len(query) < 6
-    ):  # TODO: return message that the search terms aren't ok
-        profiles = (
-            UserProfile.objects.filter(*query)
-            .select_related("user")
-            .order_by("forum_nickname")
-        )
-
-    output = []
-    for profile in profiles:
-        label = profile.forum_nickname
-        output.append({"id": profile.user.id, "label": label, "value": ""})
-    return HttpResponse(json.dumps(output))
+from .models import Announcement
 
 
 class AnnouncementView(View):
--- src/brouwers/general/urls.py
+++ src/brouwers/general/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 
-from .ajax_views import AnnouncementView, search_users
+from .ajax_views import AnnouncementView
 
 app_name = "general"
 urlpatterns = [
-    path("user/search/", search_users),
     path(
         "utils/get-announcement/", AnnouncementView.as_view(), name="get-announcement"
     ),
