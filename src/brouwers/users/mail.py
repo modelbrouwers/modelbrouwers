56c2072edc040a8dfe96f30460e432310dd2fb24
Sergei Maertens <sergeimaertens@gmail.com>
2026-06-29 08:41:35 +0200

    :sparkles: Communicate account status in registration email

--- src/brouwers/users/mail.py
+++ src/brouwers/users/mail.py
@@ -3,13 +3,17 @@ from django.utils.translation import gettext_lazy as _
 
 from brouwers.general.mail import MultiAlternativesEmail
 
+from .models import User
+
 
 class UserRegistrationEmail(MultiAlternativesEmail):
     template_name = "users/mail/user_registered"
     subject = _("Your registration on {domain}")
 
-    def __init__(self, to=None, **kwargs):
-        self.user = kwargs.get("user")
+    user: User
+
+    def __init__(self, user: User, to=None, **kwargs):
+        self.user = user
         super().__init__(to=self.user.email, **kwargs)
 
     def get_subject(self):
@@ -17,6 +21,18 @@ class UserRegistrationEmail(MultiAlternativesEmail):
         return self.subject.format(domain=domain.title())
 
     def get_context_data(self, **kwargs):
-        context = {"user": self.user}
-        kwargs.update(context)
+        status_notice = (
+            _("Your account has been activated - you can start using it immediately.")
+            if self.user.is_active
+            else _(
+                "Your account was created, but must be approved by an administrator "
+                "before you can use it."
+            )
+        )
+        kwargs.update(
+            {
+                "user": self.user,
+                "status_notice": status_notice,
+            }
+        )
         return super().get_context_data(**kwargs)
